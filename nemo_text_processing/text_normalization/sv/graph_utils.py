53eee628112a7a18f7c67332c5bb2d32d05340f4
Jim O'Regan <joregan@kth.se>
2023-01-02 23:43:05 +0100

    add TO_UPPER for (extended) Swedish alphabet instead of using ASCII
    Signed-off-by: Jim O'Regan <joregan@kth.se>

---
+++ nemo_text_processing/text_normalization/sv/graph_utils.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2023, Jim O'Regan for Språkbanken Tal
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+_ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖÜÉ"
+_ALPHA_LOWER = "abcdefghijklmnopqrstuvwxyzåäöüé"
+
+TO_LOWER = pynini.union(*[pynini.cross(x, y) for x, y in zip(_ALPHA_UPPER, _ALPHA_LOWER)])
+TO_UPPER = pynini.invert(TO_LOWER)
