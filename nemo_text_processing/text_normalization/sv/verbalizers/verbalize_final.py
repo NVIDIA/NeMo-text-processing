16c0368405144d48d3efd8114ddaa3f042a30f41
Jim O'Regan <joregan@kth.se>
2023-01-02 11:59:22 +0100

    fix cache dir
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/sv/verbalizers/verbalize_final.py
+++ nemo_text_processing/text_normalization/sv/verbalizers/verbalize_final.py
@@ -45,7 +45,7 @@ class VerbalizeFinalFst(GraphFst):
         far_file = None
         if cache_dir is not None and cache_dir != "None":
             os.makedirs(cache_dir, exist_ok=True)
-            far_file = os.path.join(cache_dir, f"es_tn_{deterministic}_deterministic_verbalizer.far")
+            far_file = os.path.join(cache_dir, f"sv_tn_{deterministic}_deterministic_verbalizer.far")
         if not overwrite_cache and far_file and os.path.exists(far_file):
             self.fst = pynini.Far(far_file, mode="r")["verbalize"]
             logging.info(f'VerbalizeFinalFst graph was restored from {far_file}.')
