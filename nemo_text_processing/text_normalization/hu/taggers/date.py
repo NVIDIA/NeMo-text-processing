526814ec1e41c3b03532c0fde89e89174d217c5e
Jim O'Regan <joregan@kth.se>
2023-01-07 13:46:12 +0000

    my copyright
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/hu/taggers/date.py
+++ nemo_text_processing/text_normalization/hu/taggers/date.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+# Copyright (c) 2023, Jim O'Regan.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
