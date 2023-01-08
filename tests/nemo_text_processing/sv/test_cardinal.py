9932ab49ba85b8366ce8858ce68d1eb83a7fa70f
Jim O'Regan <joregan@kth.se>
2023-01-08 11:17:30 +0000

    remove commented pieces, will not be done in this branch
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- tests/nemo_text_processing/sv/test_cardinal.py
+++ tests/nemo_text_processing/sv/test_cardinal.py
@@ -18,7 +18,6 @@ from parameterized import parameterized
 from ..utils import CACHE_DIR, parse_test_case_file
 
 try:
-    #    from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
     from nemo_text_processing.text_normalization.normalize import Normalizer
 
     PYNINI_AVAILABLE = True
@@ -27,21 +26,6 @@ except (ImportError, ModuleNotFoundError):
 
 
 class TestCardinal:
-
-    # inverse_normalizer = (
-    #     InverseNormalizer(lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False) if PYNINI_AVAILABLE else None
-    # )
-
-    # @parameterized.expand(parse_test_case_file('sv/data_inverse_text_normalization/test_cases_cardinal.txt'))
-    # @pytest.mark.skipif(
-    #     not PYNINI_AVAILABLE,
-    #     reason="`pynini` not installed, please install via nemo_text_processing/pynini_install.sh",
-    # )
-    # @pytest.mark.run_only_on('CPU')
-    # @pytest.mark.unit
-    # def test_denorm(self, test_input, expected):
-    #     pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
-    #     assert pred == expected
 
     normalizer = (
         Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
