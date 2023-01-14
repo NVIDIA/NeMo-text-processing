3ef604725d1e9c6abe00136a159694a9de3bed36
Jim O'Regan <joregan@kth.se>
2023-01-14 17:07:52 +0000

    change integer
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/hu/verbalizers/decimal.py
+++ nemo_text_processing/text_normalization/hu/verbalizers/decimal.py
@@ -27,11 +27,11 @@ class DecimalFst(GraphFst):
             for False multiple transduction are generated (used for audio-based normalization)
     """
 
-    def __init__(self, cardinal, deterministic: bool = True):
+    def __init__(self, deterministic: bool = True):
         super().__init__(name="decimal", kind="verbalize", deterministic=deterministic)
         self.optional_sign = pynini.cross("negative: \"true\"", "mínusz ")
         self.optional_sign = pynini.closure(self.optional_sign + delete_space, 0, 1)
-        self.integer = pynutil.delete("integer_part:") + cardinal.integer
+        self.integer = pynutil.delete("integer_part:") + pynini.closure(NEMO_NOT_QUOTE, 1) + pynutil.delete("\"")
         self.optional_integer = pynini.closure(self.integer + delete_space + insert_space, 0, 1)
         self.fractional_default = (
             pynutil.delete("fractional_part:")
