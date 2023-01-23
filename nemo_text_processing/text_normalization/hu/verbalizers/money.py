45e4b051aada911f2a30a6eecd19c22553aa7e0f
Jim O'Regan <joregan@kth.se>
2023-01-23 19:53:24 +0000

    actually saving the adaptations
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/hu/verbalizers/money.py
+++ nemo_text_processing/text_normalization/hu/verbalizers/money.py
@@ -20,12 +20,12 @@ from pynini.lib import pynutil
 class MoneyFst(GraphFst):
     """
     Finite state transducer for verbalizing money, e.g.
-        money { currency_maj: "euro" integer_part: "ein"} -> "ein euro"
-        money { currency_maj: "euro" integer_part: "eins" fractional_part: "null null eins"} -> "eins komma null null eins euro"
-        money { integer_part: "ein" currency_maj: "pfund" fractional_part: "vierzig" preserve_order: true} -> "ein pfund vierzig"
-        money { integer_part: "ein" currency_maj: "pfund" fractional_part: "vierzig" currency_min: "pence" preserve_order: true} -> "ein pfund vierzig pence"
-        money { fractional_part: "ein" currency_min: "penny" preserve_order: true} -> "ein penny"
-        money { currency_maj: "pfund" integer_part: "null" fractional_part: "null eins" quantity: "million"} -> "null komma null eins million pfund"
+        money { currency_maj: "euró" integer_part: "egy"} -> "egy euró"
+        money { currency_maj: "euró" integer_part: "egy" fractional_part: "egy ezred"} -> "egy egész egy ezred euro"
+        money { integer_part: "egy" currency_maj: "font" fractional_part: "negyven" preserve_order: true} -> "egy font negyven"
+        money { integer_part: "egy" currency_maj: "font" fractional_part: "negyven" currency_min: "penny" preserve_order: true} -> "egy font negyven penny"
+        money { fractional_part: "egy" currency_min: "penny" preserve_order: true} -> "egy penny"
+        money { currency_maj: "font" integer_part: "nulla" fractional_part: "null eins" quantity: "millió"} -> "null egész egy század millió font"
 
     Args:
         decimal: GraphFst
@@ -46,18 +46,21 @@ class MoneyFst(GraphFst):
         )
 
         integer_part = pynutil.delete("integer_part: \"") + pynini.closure(NEMO_NOT_QUOTE, 1) + pynutil.delete("\"")
-        optional_add_and = pynini.closure(pynutil.insert("und "), 0, 1)
+        optional_add_and = pynini.closure(pynutil.insert("és "), 0, 1)
 
         #  *** currency_maj
         graph_integer = integer_part + keep_space + maj
 
         #  *** currency_maj + (***) | ((und) *** current_min)
+        minor_part = fractional_part | (fractional_part + keep_space + min)
+        if not deterministic:
+            minor_part |= (optional_add_and + fractional_part + keep_space + min)
         graph_integer_with_minor = (
             integer_part
             + keep_space
             + maj
             + keep_space
-            + (fractional_part | (optional_add_and + fractional_part + keep_space + min))
+            + minor_part
             + delete_preserve_order
         )
 
