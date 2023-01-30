8fabfc092b64f4466c016df9771ba1f418429753
ekmb <ebakhturina@nvidia.com>
2023-01-29 19:12:20 -0800

    restarting ci
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo_text_processing/text_normalization/normalize.py
+++ nemo_text_processing/text_normalization/normalize.py
@@ -25,10 +25,8 @@ from math import factorial
 from time import perf_counter
 from typing import Dict, List, Optional, Union
 
-import joblib
 import pynini
 import regex
-import tqdm
 from joblib import Parallel, delayed
 from nemo_text_processing.text_normalization.data_loader_utils import (
     load_file,
