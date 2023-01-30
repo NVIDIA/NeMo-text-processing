07742d83bcf765ce100c6285c94e2fe6ccabad8c
Jim O'Regan <joregan@kth.se>
2023-01-30 10:50:11 +0000

    works in the repl, not in reality
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/hu/taggers/fraction.py
+++ nemo_text_processing/text_normalization/hu/taggers/fraction.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import pynini
-from nemo_text_processing.text_normalization.en.graph_utils import NEMO_SIGMA, GraphFst
+from nemo_text_processing.text_normalization.en.graph_utils import NEMO_DIGIT, NEMO_SIGMA, GraphFst
 from nemo_text_processing.text_normalization.hu.utils import get_abs_path
 from pynini.lib import pynutil
 
@@ -34,31 +34,6 @@ class FractionFst(GraphFst):
     def __init__(self, cardinal, ordinal, deterministic: bool = True):
         super().__init__(name="fraction", kind="classify", deterministic=deterministic)
         cardinal_graph = cardinal.graph
-        # ordinals are formed by adding -ik to the fractional, but we're working backwards
-        # Except, for some reason, this does not work.
-        # ordinal_graph = ordinal.bare_ordinals
-        ord_endings = pynini.string_file(get_abs_path("data/ordinals/endings.tsv"))
-        ord_exceptions = pynini.string_file(get_abs_path("data/ordinals/exceptional.tsv"))
-
-        exceptions = pynini.string_map([("első", "egyed"), ("második", "fél")])
-        # graph_fractional = (
-        #     ordinal_graph
-        #     @ pynini.cdrewrite(exceptions, "[BOS]", "[EOS]", NEMO_SIGMA)
-        #     @ pynini.cdrewrite(pynutil.delete("ik"), "", "[EOS]", NEMO_SIGMA)
-        # )
-        graph_fractional = (
-            (
-                cardinal_graph
-                @ pynini.cdrewrite(ord_exceptions, "[BOS]", "[EOS]", NEMO_SIGMA)
-                @ pynini.cdrewrite(ord_endings, "", "[EOS]", NEMO_SIGMA)
-            )
-            @ pynini.cdrewrite(exceptions, "[BOS]", "[EOS]", NEMO_SIGMA)
-            @ pynini.cdrewrite(pynutil.delete("ik"), "", "[EOS]", NEMO_SIGMA)
-        )
-        if not deterministic:
-            graph_fractional |= pynini.cross("2", "ketted")
-
-        self.fractional = graph_fractional.optimize()
 
         self.optional_graph_negative = pynini.closure(
             pynutil.insert("negative: ") + pynini.cross("-", "\"true\" "), 0, 1
@@ -67,7 +42,7 @@ class FractionFst(GraphFst):
         self.numerator = (
             pynutil.insert("numerator: \"") + cardinal_graph + pynini.cross(pynini.union("/", " / "), "\" ")
         )
-        self.denominator = pynutil.insert("denominator: \"") + self.fractional + pynutil.insert("\"")
+        self.denominator = pynutil.insert("denominator: \"") + ordinal.fractional + pynutil.insert("\"")
 
         self.graph = (
             self.optional_graph_negative
