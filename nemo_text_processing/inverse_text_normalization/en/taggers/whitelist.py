69304a617bdc9c872b7dbf43fee29e66e8109db3
ekmb <ebakhturina@nvidia.com>
2023-02-02 15:33:37 -0800

    add whitelist to export
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/en/taggers/whitelist.py
+++ nemo_text_processing/inverse_text_normalization/en/taggers/whitelist.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+
 import pynini
 from nemo_text_processing.inverse_text_normalization.en.utils import get_abs_path
 from nemo_text_processing.text_normalization.en.graph_utils import GraphFst, convert_space
