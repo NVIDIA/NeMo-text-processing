6c0dbbea4eb9b9da3d01bb78caacd07782de0b2e
Jim O'Regan <joregan@kth.se>
2023-02-07 11:31:10 +0000

    off by one
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/inverse_text_normalization/sv/taggers/time.py
+++ nemo_text_processing/inverse_text_normalization/sv/taggers/time.py
@@ -44,13 +44,15 @@ class TimeFst(GraphFst):
         minutes_inverse = pynini.invert(minutes @ tn_cardinal_tagger.graph_en)
         minute_words_to_words = minutes_inverse @ minutes_to @ tn_cardinal_tagger.graph_en
         minute_words_to_words = pynutil.insert("minutes: \"") + minute_words_to_words + pynutil.insert("\"")
-        def hours_to():
-            for x in range(1, 12):
+        def hours_to_pairs():
+            for x in range(1, 13):
                 if x == 12:
                     y = 1
                 else:
                     y = x + 1
                 yield x, y
+        hours_to = pynini.string_map([(str(x[0]), str(x[1])) for x in hours_to_pairs()])
+
         
 
         # lazy way to make sure compounds work
