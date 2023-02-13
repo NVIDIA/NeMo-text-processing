40da227d53152cc7a1b7abd7ba5927f8e48a5f98
Jim O'Regan <joregan@kth.se>
2023-02-13 14:33:17 +0000

    codeql
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/hu/verbalizers/date.py
+++ nemo_text_processing/text_normalization/hu/verbalizers/date.py
@@ -15,11 +15,9 @@
 import pynini
 from nemo_text_processing.text_normalization.en.graph_utils import (
     NEMO_NOT_QUOTE,
-    NEMO_SIGMA,
     GraphFst,
     delete_preserve_order,
 )
-from nemo_text_processing.text_normalization.hu.utils import get_abs_path, load_labels
 from pynini.lib import pynutil
 
 
