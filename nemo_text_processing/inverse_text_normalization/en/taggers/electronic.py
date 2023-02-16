efcdd126242474a2187cf4650223205596e201b3
Evelina <10428420+ekmb@users.noreply.github.com>
2023-02-16 12:42:49 -0800

    update ITN to work after Punctuation capitalization model (#22)
    * add cases with capitalization, cardinal, decimal pass
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix telephone, ordinal
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * restarting ci
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * restarting ci
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * restarting ci
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * restarting ci
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * update electronic
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * review feedback, update whitelist
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * rename capitalize func
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix SH tests
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix tests
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * update jenkins folder name
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * added cased arg to ITN
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * add input_case arg to other lang
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * jenkins dirs update
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * update test
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * update test
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix codeql errors
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * fix sh
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * review
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * update jenkins dir
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix default value
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    
    ---------
    
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/inverse_text_normalization/en/taggers/electronic.py
+++ nemo_text_processing/inverse_text_normalization/en/taggers/electronic.py
@@ -14,8 +14,17 @@
 # limitations under the License.
 
 import pynini
-from nemo_text_processing.inverse_text_normalization.en.utils import get_abs_path
-from nemo_text_processing.text_normalization.en.graph_utils import NEMO_ALPHA, GraphFst, insert_space
+from nemo_text_processing.inverse_text_normalization.en.utils import get_abs_path, get_various_formats
+from nemo_text_processing.text_normalization.en.graph_utils import (
+    INPUT_CASED,
+    INPUT_LOWER_CASED,
+    MIN_POS_WEIGHT,
+    NEMO_ALPHA,
+    GraphFst,
+    capitalized_input_graph,
+    insert_space,
+)
+from nemo_text_processing.text_normalization.en.utils import load_labels
 from pynini.lib import pynutil
 
 
@@ -23,44 +32,74 @@ class ElectronicFst(GraphFst):
     """
     Finite state transducer for classifying electronic: as URLs, email addresses, etc.
         e.g. c d f one at a b c dot e d u -> tokens { electronic { username: "cdf1" domain: "abc.edu" } }
+
+    Args:
+        input_case: accepting either "lower_cased" or "cased" input.
     """
 
-    def __init__(self):
+    def __init__(self, input_case: str = INPUT_LOWER_CASED):
         super().__init__(name="electronic", kind="classify")
 
         delete_extra_space = pynutil.delete(" ")
-        alpha_num = (
-            NEMO_ALPHA
-            | pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
-            | pynini.string_file(get_abs_path("data/numbers/zero.tsv"))
+
+        num = pynini.string_file(get_abs_path("data/numbers/digit.tsv")) | pynini.string_file(
+            get_abs_path("data/numbers/zero.tsv")
         )
+
+        if input_case == INPUT_CASED:
+            num = capitalized_input_graph(num)
+
+        alpha_num = (NEMO_ALPHA | num).optimize()
 
         url_symbols = pynini.string_file(get_abs_path("data/electronic/url_symbols.tsv")).invert()
         accepted_username = alpha_num | url_symbols
         process_dot = pynini.cross("dot", ".")
+        alternative_dot = (
+            pynini.closure(delete_extra_space, 0, 1) + pynini.accep(".") + pynini.closure(delete_extra_space, 0, 1)
+        )
         username = (alpha_num + pynini.closure(delete_extra_space + accepted_username)) | pynutil.add_weight(
             pynini.closure(NEMO_ALPHA, 1), weight=0.0001
         )
         username = pynutil.insert("username: \"") + username + pynutil.insert("\"")
         single_alphanum = pynini.closure(alpha_num + delete_extra_space) + alpha_num
-        server = single_alphanum | pynini.string_file(get_abs_path("data/electronic/server_name.tsv"))
-        domain = single_alphanum | pynini.string_file(get_abs_path("data/electronic/domain.tsv"))
+        server = (
+            single_alphanum
+            | pynini.string_file(get_abs_path("data/electronic/server_name.tsv"))
+            | pynini.closure(NEMO_ALPHA, 2)
+        )
+
+        if input_case == INPUT_CASED:
+            domain = []
+            # get domain formats
+            for d in load_labels(get_abs_path("data/electronic/domain.tsv")):
+                domain.extend(get_various_formats(d[0]))
+            domain = pynini.string_map(domain).optimize()
+        else:
+            domain = pynini.string_file(get_abs_path("data/electronic/domain.tsv"))
+
+        domain = single_alphanum | domain | pynini.closure(NEMO_ALPHA, 2)
         domain_graph = (
             pynutil.insert("domain: \"")
             + server
-            + delete_extra_space
-            + process_dot
-            + delete_extra_space
+            + ((delete_extra_space + process_dot + delete_extra_space) | alternative_dot)
             + domain
             + pynutil.insert("\"")
         )
         graph = username + delete_extra_space + pynutil.delete("at") + insert_space + delete_extra_space + domain_graph
 
         ############# url ###
-        protocol_end = pynini.cross(pynini.union("w w w", "www"), "www")
-        protocol_start = (pynini.cross("h t t p", "http") | pynini.cross("h t t p s", "https")) + pynini.cross(
-            " colon slash slash ", "://"
-        )
+        if input_case == INPUT_CASED:
+            protocol_end = pynini.cross(pynini.union(*get_various_formats("www")), "www")
+
+            protocol_start = pynini.cross(pynini.union(*get_various_formats("http")), "http") | pynini.cross(
+                pynini.union(*get_various_formats("https")), "https"
+            )
+        else:
+            protocol_end = pynini.cross(pynini.union("w w w", "www"), "www")
+            protocol_start = pynini.cross("h t t p", "http") | pynini.cross("h t t p s", "https")
+
+        protocol_start += pynini.cross(" colon slash slash ", "://")
+
         # .com,
         ending = (
             delete_extra_space
@@ -80,11 +119,18 @@ class ElectronicFst(GraphFst):
             pynini.closure(protocol_start, 0, 1) + protocol_end + delete_extra_space + process_dot + protocol_default
         ).optimize()
 
+        if input_case == INPUT_CASED:
+            protocol |= (
+                pynini.closure(protocol_start, 0, 1) + protocol_end + alternative_dot + protocol_default
+            ).optimize()
+
         protocol |= pynini.closure(protocol_end + delete_extra_space + process_dot, 0, 1) + protocol_default
 
         protocol = pynutil.insert("protocol: \"") + protocol.optimize() + pynutil.insert("\"")
         graph |= protocol
-        ########
+
+        if input_case == INPUT_CASED:
+            graph = capitalized_input_graph(graph, capitalized_graph_weight=MIN_POS_WEIGHT)
 
         final_graph = self.add_tokens(graph)
         self.fst = final_graph.optimize()
