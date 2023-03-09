587851508b37c7e097727f5d6b6e3ec9065176a2
Evelina <10428420+ekmb@users.noreply.github.com>
2023-03-09 14:59:06 -0800

    fix sv tests (#52)
    Signed-off-by: ekmb <ebakhturina@nvidia.com>
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>

--- tests/nemo_text_processing/sv/test_money.py
+++ tests/nemo_text_processing/sv/test_money.py
@@ -18,14 +18,16 @@ from nemo_text_processing.text_normalization.normalize import Normalizer
 from nemo_text_processing.text_normalization.normalize_with_audio import NormalizerWithAudio
 from parameterized import parameterized
 
-from ..utils import CACHE_DIR, parse_test_case_file
+from ..utils import CACHE_DIR, RUN_AUDIO_BASED_TESTS, parse_test_case_file
 
 
 class TestMoney:
     normalizer = Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
 
-    normalizer_with_audio = NormalizerWithAudio(
-        input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False
+    normalizer_with_audio = (
+        NormalizerWithAudio(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
+        if RUN_AUDIO_BASED_TESTS
+        else None
     )
 
     @parameterized.expand(parse_test_case_file('sv/data_text_normalization/test_cases_money.txt'))
