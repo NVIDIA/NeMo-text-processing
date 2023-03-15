78c844ff2e24ffe91441c9f3588604026fa7750c
Jim O’Regan <jaoregan@tcd.ie>
2023-03-15 21:35:26 +0100

    add inverse
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- tests/nemo_text_processing/sv/test_word.py
+++ tests/nemo_text_processing/sv/test_word.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import pytest
+from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
 from nemo_text_processing.text_normalization.normalize import Normalizer
 from nemo_text_processing.text_normalization.normalize_with_audio import NormalizerWithAudio
 from parameterized import parameterized
@@ -21,6 +22,15 @@ from ..utils import CACHE_DIR, RUN_AUDIO_BASED_TESTS, parse_test_case_file
 
 
 class TestWord:
+    inverse_normalizer = InverseNormalizer(lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
+
+    @parameterized.expand(parse_test_case_file('sv/data_inverse_text_normalization/test_cases_word.txt'))
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_denorm(self, test_input, expected):
+        pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
+        assert pred == expected
+
     normalizer = Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
 
     normalizer_with_audio = (
