11810c59590db4fc979c6de067932da519361282
Jim O'Regan <joregan@kth.se>
2023-03-20 12:38:36 +0000

    replace with copy from sv
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/ga/verbalizers/date.py
+++ nemo_text_processing/text_normalization/ga/verbalizers/date.py
@@ -14,19 +14,20 @@
 import pynini
 from nemo_text_processing.text_normalization.en.graph_utils import (
     NEMO_NOT_QUOTE,
-    NEMO_SIGMA,
     NEMO_SPACE,
     GraphFst,
     delete_preserve_order,
 )
+from nemo_text_processing.text_normalization.sv.utils import get_abs_path
 from pynini.lib import pynutil
+
+era_words = pynini.string_file(get_abs_path("data/dates/era_words.tsv"))
 
 
 class DateFst(GraphFst):
     """
     Finite state transducer for verbalizing date, e.g.
-        date { day: "treinta y uno" month: "marzo" year: "dos mil" } -> "treinta y uno de marzo de dos mil"
-        date { day: "uno" month: "mayo" year: "del mil novecientos noventa" } -> "primero de mayo del mil novecientos noventa"
+        date { day: "trettioförsta" month: "mars" year: "tjugotjugotvå" } -> "trettioförsta mars tjugotjugotvå"
 
     Args:
         deterministic: if True will provide a single transduction option,
@@ -36,42 +37,26 @@ class DateFst(GraphFst):
     def __init__(self, deterministic: bool = True):
         super().__init__(name="date", kind="verbalize", deterministic=deterministic)
 
-        day_cardinal = pynutil.delete("day: \"") + pynini.closure(NEMO_NOT_QUOTE, 1) + pynutil.delete("\"")
-        day = strip_cardinal_apocope(day_cardinal)
-
-        primero = pynini.cdrewrite(pynini.cross("uno", "primero"), "[BOS]", "[EOS]", NEMO_SIGMA)
-        day = (
-            (day @ primero) if deterministic else pynini.union(day, day @ primero)
-        )  # Primero for first day is traditional, but will vary depending on region
+        day = pynutil.delete("day: \"") + pynini.closure(NEMO_NOT_QUOTE, 1) + pynutil.delete("\"")
 
         month = pynutil.delete("month: \"") + pynini.closure(NEMO_NOT_QUOTE, 1) + pynutil.delete("\"")
 
-        year = (
-            pynutil.delete("year: \"")
-            + articles
-            + NEMO_SPACE
-            + pynini.closure(NEMO_NOT_QUOTE, 1)
-            + pynutil.delete("\"")
-        )
+        year = pynutil.delete("year: \"") + pynini.closure(NEMO_NOT_QUOTE, 1) + pynutil.delete("\"")
 
-        # Insert preposition if wasn't originally with the year. This would mean a space was present
-        year = pynutil.add_weight(year, -0.001)
-        year |= (
-            pynutil.delete("year: \"")
-            + pynutil.insert("de ")
-            + pynini.closure(NEMO_NOT_QUOTE, 1)
-            + pynutil.delete("\"")
-        )
+        era = pynutil.delete("era: \"") + (pynini.closure(NEMO_NOT_QUOTE, 1) @ era_words) + pynutil.delete("\"")
+        era_opt = pynini.closure(NEMO_SPACE + era, 0, 1)
+        year_era_opt = year + era_opt
 
         # day month year
-        graph_dmy = day + pynini.cross(NEMO_SPACE, " de ") + month + pynini.closure(pynini.accep(" ") + year, 0, 1)
+        graph_year_era = year + NEMO_SPACE + era + delete_preserve_order
+        graph_year_era |= year + NEMO_SPACE + era
+        graph_dmy = pynini.union(
+            day + NEMO_SPACE + month + pynini.closure(NEMO_SPACE + year_era_opt, 0, 1) + delete_preserve_order
+        )
+        graph_was_ymd = pynini.union(month + NEMO_SPACE + year, day + NEMO_SPACE + month + NEMO_SPACE + year)
 
-        graph_mdy = month + NEMO_SPACE + day + pynini.closure(NEMO_SPACE + year, 0, 1)
-        if deterministic:
-            graph_mdy += pynutil.delete(" preserve_order: true")  # Only accepts this if was explicitly passed
-
-        self.graph = graph_dmy | graph_mdy
-        final_graph = self.graph + delete_preserve_order
+        self.graph = graph_dmy | graph_year_era | graph_was_ymd | year
+        final_graph = self.graph
 
         delete_tokens = self.delete_tokens(final_graph)
         self.fst = delete_tokens.optimize()
