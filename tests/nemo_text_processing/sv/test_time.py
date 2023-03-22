d2463b8bee199c1d1b60b84475a6ee0b116a5db7
Jim O'Regan <joregan@kth.se>
2023-03-22 15:32:34 +0000

    change the variable names
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- tests/nemo_text_processing/sv/test_time.py
+++ tests/nemo_text_processing/sv/test_time.py
@@ -21,9 +21,9 @@ from ..utils import CACHE_DIR, RUN_AUDIO_BASED_TESTS, parse_test_case_file
 
 
 class TestTime:
-    normalizer = Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
+    normalizer_sv = Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
 
-    normalizer_with_audio = (
+    normalizer_sv_with_audio = (
         NormalizerWithAudio(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
         if RUN_AUDIO_BASED_TESTS
         else None
@@ -33,11 +33,11 @@ class TestTime:
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_norm(self, test_input, expected):
-        pred = self.normalizer.normalize(test_input, verbose=False)
+        pred = self.normalizer_sv.normalize(test_input, verbose=False)
         assert pred == expected
 
-        if self.normalizer_with_audio:
-            pred_non_deterministic = self.normalizer_with_audio.normalize(
+        if self.normalizer_sv_with_audio:
+            pred_non_deterministic = self.normalizer_sv_with_audio.normalize(
                 test_input, n_tagged=500, punct_post_process=False
             )
             assert expected in pred_non_deterministic
