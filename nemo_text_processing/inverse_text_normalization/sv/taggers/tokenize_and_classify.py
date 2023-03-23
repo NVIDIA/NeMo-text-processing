4da069b09a6f56bcfd82ef4ebfb0e8dcde43261b
Jim O'Regan <joregan@kth.se>
2023-03-23 19:21:15 +0000

    add to t&c
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/inverse_text_normalization/sv/taggers/tokenize_and_classify.py
+++ nemo_text_processing/inverse_text_normalization/sv/taggers/tokenize_and_classify.py
@@ -131,7 +131,7 @@ class ClassifyFst(GraphFst):
                 | pynutil.add_weight(ordinal_graph, 1.1)
                 | pynutil.add_weight(fraction_graph, 1.1)
                 | pynutil.add_weight(money_graph, 1.1)
-                # | pynutil.add_weight(telephone_graph, 1.1)
+                | pynutil.add_weight(telephone_graph, 1.1)
                 | pynutil.add_weight(electronic_graph, 1.1)
                 | pynutil.add_weight(word_graph, 100)
             )
