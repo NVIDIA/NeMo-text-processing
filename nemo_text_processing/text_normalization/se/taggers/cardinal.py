c264bb83fa3c7b2f77b5fb99a150a426063b1fc6
Jim O'Regan <joregan@kth.se>
2023-03-30 14:46:56 +0000

    temporary code (will stick in the repl and write the files)
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/se/taggers/cardinal.py
+++ nemo_text_processing/text_normalization/se/taggers/cardinal.py
@@ -84,9 +84,13 @@ class CardinalFst(GraphFst):
             graph_digit |= pynini.cross("1", "akta")
 
         CASE_KEYS = ["ess", "com_pl", "com_sg", "gen_sg", "gen_pl", "ill_pl", "ill_sg", "loc_sg", "nom_pl"]
-        DIGIT_SUFFIX = {
-            a: b for a, b in zip(CASE_KEYS, [":n", ":iguin", ":in", ":a", ":id", ":ide", ":i", ":s", ":t"])
-        }
+        def caseify(items):
+            return {
+                a: b for a, b in zip(CASE_KEYS, items)
+            }
+
+        DIGIT_SUFFIX = caseify([":n", ":iguin", ":in", ":a", ":id", ":ide", ":i", ":s", ":t"])
+        CUODI_CASE = caseify(["čuođin", "čuđiiguin", "čuđiin", "čuođi", "čuđiid", "čuđiide", "čuohtái", "čuođis", "čuođit"])
 
         digits_cased = {}
         digits_cased_fst = {}
