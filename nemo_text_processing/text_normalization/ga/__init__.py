1bfd5a648f8e1554a1b2e8c9a9af8881e2e7f754
Jim O'Regan <joregan@kth.se>
2023-05-22 11:47:38 +0000

    add a dialect variable, work in that direction
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/ga/__init__.py
+++ nemo_text_processing/text_normalization/ga/__init__.py
@@ -11,3 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+DIALECT = "co" # "gm", "gc", "gu" for Munster, Connaught, and Ulster, respectively
