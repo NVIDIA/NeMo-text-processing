db1179e5d503c4b49537eb519906ecb9f2ef8936
BuyuanCui <alexcui1994@gmail.com>
2023-06-06 09:14:35 -0700

    commenting out this file but will keep it for future release
    Signed-off-by: BuyuanCui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/zh/test_math.py
+++ tests/nemo_text_processing/zh/test_math.py
@@ -19,12 +19,12 @@ from parameterized import parameterized
 from ..utils import CACHE_DIR, parse_test_case_file
 
 
-class TestMath:
-    normalizer_zh = Normalizer(lang='zh', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
-
-    @parameterized.expand(parse_test_case_file('zh/data_text_normalization/test_cases_math.txt'))
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_norm_math(self, test_input, expected):
-        preds = self.normalizer_zh.normalize(test_input)
-        assert expected == preds
+#class TestMath:
+#    normalizer_zh = Normalizer(lang='zh', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
+#
+#    @parameterized.expand(parse_test_case_file('zh/data_text_normalization/test_cases_math.txt'))
+#    @pytest.mark.run_only_on('CPU')
+#    @pytest.mark.unit
+#    def test_norm_math(self, test_input, expected):
+#        preds = self.normalizer_zh.normalize(test_input)
+#        assert expected == preds
