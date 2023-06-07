923727b1d124875b4ae7360c261092b0a81ffc96
Jim O'Regan <joregan@kth.se>
2023-06-07 11:30:10 +0000

    redo
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- nemo_text_processing/text_normalization/se/utils.py
+++ nemo_text_processing/text_normalization/se/utils.py
@@ -18,6 +18,7 @@ import logging
 import os
 
 CASE_KEYS = ["ess", "com_pl", "com_sg", "gen_sg", "gen_pl", "ill_pl", "ill_sg", "loc_sg", "nom_pl"]
+CASE_KEYS_EXT = CASE_KEYS + ["nom_sg"]
 
 
 def get_abs_path(rel_path):
@@ -50,7 +51,7 @@ def load_labels(abs_path):
         return labels
 
 
-def load_case_forms(abs_path):
+def load_case_forms(abs_path, extended=False):
     """
     loads relative path file as dictionary, keyed on case/number
 
@@ -59,10 +60,13 @@ def load_case_forms(abs_path):
 
     Returns dictionary of case forms
     """
+    KEYS = CASE_KEYS
+    if extended:
+        KEYS = CASE_KEYS_EXT
     with open(abs_path, encoding="utf-8") as label_tsv:
         labels = list(csv.reader(label_tsv, delimiter="\t"))
         ret = {}
         for label in labels:
-            if label and label[0] in CASE_KEYS:
+            if label and (label[0] in KEYS):
                 ret[label[0]] = label[1]
         return ret
