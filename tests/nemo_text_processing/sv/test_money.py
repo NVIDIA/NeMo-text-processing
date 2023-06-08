8a66863ba6db3058aaceaa070168ac1bc0dc557a
Jim O'Regan <joregan@kth.se>
2023-06-08 13:11:08 +0000

    remove unused test pieces
    Signed-off-by: Jim O'Regan <joregan@kth.se>

--- tests/nemo_text_processing/sv/test_money.py
+++ tests/nemo_text_processing/sv/test_money.py
@@ -14,7 +14,6 @@
 
 
 import pytest
-from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
 from nemo_text_processing.text_normalization.normalize import Normalizer
 from nemo_text_processing.text_normalization.normalize_with_audio import NormalizerWithAudio
 from parameterized import parameterized
@@ -23,21 +22,6 @@ from ..utils import CACHE_DIR, RUN_AUDIO_BASED_TESTS, parse_test_case_file
 
 
 class TestMoney:
-    inverse_normalizer_sv = InverseNormalizer(lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
-    inverse_normalizer_sv_cased = InverseNormalizer(
-        lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False, input_case="cased"
-    )
-
-    @parameterized.expand(parse_test_case_file('sv/data_inverse_text_normalization/test_cases_money.txt'))
-    @pytest.mark.run_only_on('CPU')
-    @pytest.mark.unit
-    def test_denorm(self, test_input, expected):
-        pred = self.inverse_normalizer_sv.inverse_normalize(test_input, verbose=False)
-        assert pred == expected
-
-        pred = self.inverse_normalizer_sv_cased.inverse_normalize(test_input, verbose=False)
-        assert pred == expected
-
     normalizer_sv = Normalizer(input_case='cased', lang='sv', cache_dir=CACHE_DIR, overwrite_cache=False)
 
     normalizer_sv_with_audio = (
