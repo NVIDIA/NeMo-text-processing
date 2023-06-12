d3f2ef10983df1a62656c97580b53bad198d4098
Jim O'Regan <joregan@kth.se>
2023-06-12 09:43:55 +0000

    Revert "delete line"
    This reverts commit ec2e369cb8cf3dfddbfbcceebfcb39fe1b3943f2.

--- nemo_text_processing/text_normalization/pl/taggers/cardinal.py
+++ nemo_text_processing/text_normalization/pl/taggers/cardinal.py
@@ -111,6 +111,7 @@ def filter_punctuation(fst: 'pynini.FstLike') -> 'pynini.FstLike':
 
     return cardinal_string @ fst
 
+
 class CardinalFst(GraphFst):
     """
     Finite state transducer for classifying cardinals, e.g.
