e6587a85c10b24d6e35b61469316f52a3e3d5a7e
Jim O’Regan <joregan@kth.se>
2023-06-14 13:46:54 +0200

    Update date.py
    Signed-off-by: Jim O’Regan <joregan@kth.se>

--- nemo_text_processing/inverse_text_normalization/sv/taggers/date.py
+++ nemo_text_processing/inverse_text_normalization/sv/taggers/date.py
@@ -47,8 +47,7 @@ class DateFst(GraphFst):
         # day_double = force_double_digits(tn_date_tagger.digit_day).invert().optimize()
         month_double = force_double_digits(tn_date_tagger.number_to_month).invert().optimize()
         month_abbr = tn_date_tagger.month_abbr.invert().optimize()
-        month = tn_date_tagger.number_to_month.invert().optimize()
-        self.month_to_number = month
+        self.month_to_number = tn_date_tagger.number_to_month.invert().optimize()
 
         graph_year = pynutil.insert("year: \"") + year + pynutil.insert("\"")
         graph_month = pynutil.insert("month: \"") + month_double + pynutil.insert("\"")
