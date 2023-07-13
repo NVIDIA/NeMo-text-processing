daeafa24d7cc2e6c3afeded3b75b80f10eb753d2
BuyuanCui <alexcui1994@gmail.com>
2023-07-13 04:50:09 -0700

    test python file edits
    Signed-off-by: BuyuanCui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/zh/test_decimal.py
+++ tests/nemo_text_processing/zh/test_decimal.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,17 +14,27 @@
 
 import pytest
 from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
+from nemo_text_processing.text_normalization.normalize import Normalizer
 from parameterized import parameterized
 
 from ..utils import CACHE_DIR, parse_test_case_file
 
 
-class TestDecimal:
-    inverse_normalizer = InverseNormalizer(lang='zh', cache_dir=CACHE_DIR, overwrite_cache=False)
+class TestDate:
+    normalizer_zh = Normalizer(lang='zh', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
 
-    @parameterized.expand(parse_test_case_file('zh/data_inverse_text_normalization/test_cases_decimal.txt'))
+    @parameterized.expand(parse_test_case_file('zh/data_text_normalization/test_cases_decimal.txt'))
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
-    def test_denorm(self, test_input, expected):
-        pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
-        assert pred == expected
+    def test_norm_date(self, test_input, expected):
+        preds = self.normalizer_zh.normalize(test_input)
+        assert expected == preds
+
+    # inverse_normalizer = InverseNormalizer(lang='zh', cache_dir=CACHE_DIR, overwrite_cache=False)
+
+    # @parameterized.expand(parse_test_case_file('zh/data_inverse_text_normalization/test_cases_date.txt'))
+    # @pytest.mark.run_only_on('CPU')
+    # @pytest.mark.unit
+    # def test_denorm(self, test_input, expected):
+    #    pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
+    #    assert pred == expected
