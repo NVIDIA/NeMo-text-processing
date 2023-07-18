6a0ed301fc05dccf5047e5f4ee0415e80e37549c
Mariana Graterol Fuenmayor <mgrafu@gmail.com>
2023-07-18 10:07:18 -0700

    rebase branch
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>

--- nemo_text_processing/inverse_text_normalization/es/taggers/cardinal.py
+++ nemo_text_processing/inverse_text_normalization/es/taggers/cardinal.py
@@ -15,12 +15,7 @@
 
 import pynini
 from nemo_text_processing.inverse_text_normalization.es.utils import get_abs_path
-from nemo_text_processing.text_normalization.en.graph_utils import (
-    NEMO_DIGIT,
-    NEMO_SPACE,
-    GraphFst,
-    delete_space,
-)
+from nemo_text_processing.text_normalization.en.graph_utils import NEMO_DIGIT, NEMO_SPACE, GraphFst, delete_space
 from pynini.lib import pynutil
 
 
