00a3ea3481c0c655d00a16da3d37eb2111f71be4
BuyuanCui <alexcui1994@gmail.com>
2023-07-31 11:40:33 -0700

    removed unsed imports
    Signed-off-by: BuyuanCui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/zh/verbalizers/money.py
+++ nemo_text_processing/text_normalization/zh/verbalizers/money.py
@@ -15,7 +15,6 @@
 
 import pynini
 from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_NOT_QUOTE, GraphFst, delete_space
-from nemo_text_processing.text_normalization.zh.utils import get_abs_path
 from pynini.lib import pynutil
 
 
