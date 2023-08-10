a13e86dd94bbe3ddc3c3db65dd43443dc68eb8c0
BuyuanCui <alexcui1994@gmail.com>
2023-08-10 13:08:23 -0700

    added jp
    Signed-off-by: BuyuanCui <alexcui1994@gmail.com>

--- nemo_text_processing/inverse_text_normalization/inverse_normalize.py
+++ nemo_text_processing/inverse_text_normalization/inverse_normalize.py
@@ -112,6 +112,12 @@ class InverseNormalizer(Normalizer):
                 VerbalizeFinalFst,
             )
 
+        elif lang == 'jp':  # Mandarin
+            from nemo_text_processing.inverse_text_normalization.jp.taggers.tokenize_and_classify import ClassifyFst
+            from nemo_text_processing.inverse_text_normalization.jp.verbalizers.verbalize_final import (
+                VerbalizeFinalFst,
+            )
+
         self.tagger = ClassifyFst(
             cache_dir=cache_dir, whitelist=whitelist, overwrite_cache=overwrite_cache, input_case=input_case
         )
@@ -155,7 +161,7 @@ def parse_args():
     parser.add_argument(
         "--language",
         help="language",
-        choices=['en', 'de', 'es', 'pt', 'ru', 'fr', 'vi', 'ar', 'es_en', 'zh'],
+        choices=['en', 'de', 'es', 'pt', 'ru', 'fr', 'vi', 'ar', 'es_en', 'zh', 'jp'],
         default="en",
         type=str,
     )
