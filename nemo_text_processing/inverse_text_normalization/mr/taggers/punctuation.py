a2846c0fe76ce232dde650b6358195299638539b
Chinmay Patil <chinmaypatil2000@gmail.com>
2023-08-30 20:01:06 +0530

    Added Punctuation Fst and updated tokenize_and_classify

--- nemo_text_processing/inverse_text_normalization/mr/taggers/punctuation.py
+++ nemo_text_processing/inverse_text_normalization/mr/taggers/punctuation.py
@@ -14,17 +14,22 @@
 # limitations under the License.
 
 import pynini
-from nemo_text_processing.text_normalization.en.graph_utils import NEMO_NOT_SPACE, GraphFst
+from nemo_text_processing.text_normalization.en.graph_utils import GraphFst
 from pynini.lib import pynutil
 
 
-class WordFst(GraphFst):
+class PunctuationFst(GraphFst):
     """
-    Finite state transducer for classifying plain tokens, that do not belong to any special class. This can be considered as the default class.
-        e.g. sleep -> tokens { name: "sleep" }
+    Finite state transducer for classifying punctuation
+        e.g. a, -> tokens { name: "a" } tokens { name: "," }
     """
 
     def __init__(self):
-        super().__init__(name="word", kind="classify")
-        word = pynutil.insert("name: \"") + pynini.closure(NEMO_NOT_SPACE, 1) + pynutil.insert("\"")
-        self.fst = word.optimize()
+        super().__init__(name="punctuation", kind="classify")
+
+        s = "!#$%&\'()*+,-./:;<=>?@^_`{|}~"
+        punct = pynini.union(*s)
+
+        graph = pynutil.insert("name: \"") + punct + pynutil.insert("\"")
+
+        self.fst = graph.optimize()
