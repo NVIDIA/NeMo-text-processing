24a96dcd95e9c83d083476807a437386fd426196
Chinmay Patil <chinmaypatil2000@gmail.com>
2023-09-16 19:32:28 +0530

    Completed Cardinals with all possibilities
    Signed-off-by: Chinmay Patil <chinmaypatil2000@gmail.com>

--- nemo_text_processing/inverse_text_normalization/mr/taggers/cardinal.py
+++ nemo_text_processing/inverse_text_normalization/mr/taggers/cardinal.py
@@ -41,12 +41,78 @@ class CardinalFst(GraphFst):
 
     def __init__(self):
         super().__init__(name="cardinal", kind="classify")
-        graph_digits = pynini.string_file(get_abs_path("data/numbers/digits.tsv"))
-        graph_numbers = pynini.string_file(get_abs_path("data/numbers/tens.tsv"))
-        graph_thousands = pynini.string_file(get_abs_path("data/numbers/thousands.tsv"))
+        graph_zero = pynini.string_file("/content/zero.tsv")
+        graph_digits = pynini.string_file("/content/digits.tsv")
+        graph_thousands = pynini.string_file("/content/thousands.tsv")
+        graph_tens = pynini.string_file("/content/tens.tsv")
 
-        final_graph = graph_digits | graph_numbers | graph_thousands
-        final_graph = pynutil.insert("integer: \"") + final_graph + pynutil.insert("\"")
+        graph_hundred = pynini.cross("शे", "")
+
+        graph_hundred_component = pynini.union(graph_digits + graph_hundred, pynutil.insert("०"))
+        graph_hundred_component += delete_space
+        graph_hundred_component += pynini.union(
+            pynutil.insert("००"),
+            graph_tens,
+            pynutil.insert("०") + graph_digits
+        )
+
+        graph_hundred_component_at_least_one_non_zero_digit = graph_hundred_component @ (
+                pynini.closure(NEMO_DIGIT) + (NEMO_DIGIT - "०") + pynini.closure(NEMO_DIGIT)
+        )
+        self.graph_hundred_component_at_least_one_non_zero_digit = (graph_hundred_component_at_least_one_non_zero_digit)
+
+        # eleven hundred -> 1100 etc form
+        graph_hundred_as_thousand = graph_tens + graph_hundred
+        graph_hundred_as_thousand += delete_space + pynini.union(
+            pynutil.insert("००"),
+            graph_tens,
+            pynutil.insert("०") + graph_digits
+        )
+
+        graph_hundreds = graph_hundred_component | graph_hundred_as_thousand
+
+        graph_two_digit_component = pynini.union(
+            pynutil.insert("००"),
+            graph_tens,
+            pynutil.insert("०") + graph_digits
+        )
+
+        graph_two_digit_component_at_least_one_non_zero_digit = graph_two_digit_component @ (
+                pynini.closure(NEMO_DIGIT) + (NEMO_DIGIT - "०") + pynini.closure(NEMO_DIGIT)
+        )
+        self.graph_two_digit_component_at_least_one_non_zero_digit = graph_two_digit_component_at_least_one_non_zero_digit
+
+        graph_thousands = pynini.union(
+            graph_two_digit_component_at_least_one_non_zero_digit + delete_space + pynutil.delete("हजार"),
+            pynutil.insert("००", weight=0.1)
+        )
+
+        graph_lakhs = pynini.union(
+            graph_two_digit_component_at_least_one_non_zero_digit + delete_space + pynutil.delete("लाख"),
+            pynutil.insert("००", weight=0.1)
+        )
+
+        graph_crores = pynini.union(
+            graph_two_digit_component_at_least_one_non_zero_digit + delete_space + pynutil.delete("कोटी"),
+            pynutil.insert("००", weight=0.1)
+        )
+
+        graph_arabs = pynini.union(
+            graph_two_digit_component_at_least_one_non_zero_digit + delete_space + pynutil.delete("अब्ज"),
+            pynutil.insert("००", weight=0.1)
+        )
+
+        graph_higher_powers = (graph_arabs + delete_space + graph_crores + delete_space +
+                               graph_lakhs + delete_space + graph_thousands)
+
+        graph = pynini.union(graph_higher_powers + delete_space + graph_hundreds, graph_zero, )
+        graph = graph @ pynini.union(
+            pynutil.delete(pynini.closure("०")) + pynini.difference(NEMO_DIGIT, "०") + pynini.closure(NEMO_DIGIT), "०"
+        )
+        graph = graph.optimize()
+
+        self.graph = (pynini.project(graph, "input")) @ graph
+
+        final_graph = pynutil.insert("integer: \"") + graph + pynutil.insert("\"")
         final_graph = self.add_tokens(final_graph)
-        self.fst = final_graph
-
+        self.fst = final_graph.optimize()
