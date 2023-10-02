23b8b693037573d9a848ae47b0a2a28a8885096a
BuyuanCui <alexcui1994@gmail.com>
2023-10-02 08:17:00 -0700

    removed unsed imports
    Signed-off-by: BuyuanCui <alexcui1994@gmail.com>

--- nemo_text_processing/inverse_text_normalization/jp/taggers/whitelist.py
+++ nemo_text_processing/inverse_text_normalization/jp/taggers/whitelist.py
@@ -15,7 +15,7 @@
 
 
 import pynini
-from nemo_text_processing.inverse_text_normalization.jp.graph_utils import INPUT_LOWER_CASED, GraphFst
+from nemo_text_processing.inverse_text_normalization.jp.graph_utils import GraphFst
 from nemo_text_processing.inverse_text_normalization.jp.utils import get_abs_path
 from pynini.lib import pynutil
 
