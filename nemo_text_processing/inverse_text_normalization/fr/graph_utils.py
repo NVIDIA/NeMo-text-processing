0bfa4ab59afea5a0e918be6366789b12889b847a
Evelina <10428420+ekmb@users.noreply.github.com>
2023-10-24 07:45:53 -0700

    Logging clean up + IT TN fix (#118)
    * fix utils and it TN
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * clean up
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * fix logging
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * add IT TN to CI
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * update patch
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    ---------
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo_text_processing/inverse_text_normalization/fr/graph_utils.py
+++ nemo_text_processing/inverse_text_normalization/fr/graph_utils.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 import os
 import string
 from pathlib import Path
@@ -21,6 +20,7 @@ from typing import Dict
 
 import pynini
 from nemo_text_processing.inverse_text_normalization.fr.utils import get_abs_path
+from nemo_text_processing.utils.logging import logger
 from pynini import Far
 from pynini.examples import plurals
 from pynini.export import export
@@ -80,7 +80,7 @@ def generator_main(file_name: str, graphs: Dict[str, pynini.FstLike]):
     for rule, graph in graphs.items():
         exporter[rule] = graph.optimize()
     exporter.close()
-    logging.info(f'Created {file_name}')
+    logger.info(f'Created {file_name}')
 
 
 def get_plurals(fst):
