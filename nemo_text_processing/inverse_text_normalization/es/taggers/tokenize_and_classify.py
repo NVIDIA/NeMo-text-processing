6e28247b3a57c977d680aa0a9e797134248a4b9d
Evelina <10428420+ekmb@users.noreply.github.com>
2023-10-24 07:45:53 -0700

    Logging clean up + IT TN fix (#118)
    * fix utils and it TN
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * clean up
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * fix logging
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * add IT TN to CI
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * update patch
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    ---------
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo_text_processing/inverse_text_normalization/es/taggers/tokenize_and_classify.py
+++ nemo_text_processing/inverse_text_normalization/es/taggers/tokenize_and_classify.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 import os
 
 import pynini
@@ -36,6 +35,7 @@ from nemo_text_processing.text_normalization.en.graph_utils import (
     delete_space,
     generator_main,
 )
+from nemo_text_processing.utils.logging import logger
 from pynini.lib import pynutil
 
 
@@ -67,9 +67,9 @@ class ClassifyFst(GraphFst):
             far_file = os.path.join(cache_dir, f"es_itn_{input_case}.far")
         if not overwrite_cache and far_file and os.path.exists(far_file):
             self.fst = pynini.Far(far_file, mode="r")["tokenize_and_classify"]
-            logging.info(f"ClassifyFst.fst was restored from {far_file}.")
+            logger.info(f"ClassifyFst.fst was restored from {far_file}.")
         else:
-            logging.info(f"Creating ClassifyFst grammars.")
+            logger.info(f"Creating ClassifyFst grammars.")
 
             cardinal = CardinalFst()
             cardinal_graph = cardinal.fst
@@ -121,4 +121,3 @@ class ClassifyFst(GraphFst):
 
             if far_file:
                 generator_main(far_file, {"tokenize_and_classify": self.fst})
-                logging.info(f"ClassifyFst grammars are saved to {far_file}.")
