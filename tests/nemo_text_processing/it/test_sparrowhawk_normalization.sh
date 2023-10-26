7db87344638e1bf3cb6cd458e45997444806a126
Mariana <47233618+mgrafu@users.noreply.github.com>
2023-10-26 15:45:30 -0400

    IT TN improvement on tests (#120)
    * add missing test cases
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    
    * fix bug with time tests
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    
    * update ci date
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    
    * add sentence test cases
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    
    * refine shortest path for irregular cardinals
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    
    * update ci date
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    
    ---------
    
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>

--- tests/nemo_text_processing/it/test_sparrowhawk_normalization.sh
+++ tests/nemo_text_processing/it/test_sparrowhawk_normalization.sh
@@ -8,10 +8,14 @@ runtest () {
 
   # read test file
   while read testcase; do
-    IFS='~' read written spoken <<< $testcase
-    # replace non breaking space with breaking space
-    denorm_pred=$(echo $written | normalizer_main --config=sparrowhawk_configuration.ascii_proto 2>&1 | tail -n 1)
+    IFS='~' read -a testcase_tokenized <<< $testcase
+    written=${testcase_tokenized[0]}
+    # only tests against first possible option when there are multiple shortest paths
+    spoken=${testcase_tokenized[1]}
 
+    # replace non breaking space with breaking space
+    denorm_pred=$(echo $written | normalizer_main --config=sparrowhawk_configuration.ascii_proto 2>&1 | tail -n 1 | sed 's/\xC2\xA0/ /g')
+    
     # trim white space
     spoken="$(echo -e "${spoken}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
     denorm_pred="$(echo -e "${denorm_pred}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
