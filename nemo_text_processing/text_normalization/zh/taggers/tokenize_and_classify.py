41174dad1f70a4b948ea1429095547ceae99319d
Alex Cui <alexcui1994@gmail.com>
2023-12-12 14:44:52 -0800

    fixings according to ci
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/zh/taggers/tokenize_and_classify.py
+++ nemo_text_processing/text_normalization/zh/taggers/tokenize_and_classify.py
@@ -71,7 +71,6 @@ class ClassifyFst(GraphFst):
             decimal = DecimalFst(cardinal=cardinal, deterministic=deterministic)
             word = WordFst(deterministic=deterministic)
             fraction = FractionFst(cardinal=cardinal, deterministic=deterministic)
-            # math_symbol = MathSymbol(cardinal=cardinal, deterministic=deterministic)
             money = MoneyFst(cardinal=cardinal, deterministic=deterministic)
             measure = MeasureFst(cardinal=cardinal, decimal=decimal, fraction=fraction, deterministic=deterministic)
             time = TimeFst(deterministic=deterministic)
@@ -79,16 +78,15 @@ class ClassifyFst(GraphFst):
             ordinal = OrdinalFst(cardinal=cardinal, deterministic=deterministic)
 
             classify = pynini.union(
-                # pynutil.add_weight(date.fst, 1.1),
+                pynutil.add_weight(date.fst, 1.1),
                 pynutil.add_weight(fraction.fst, -1.0),
-                # pynutil.add_weight(money.fst, 1.1),
+                pynutil.add_weight(money.fst, 1.1),
                 pynutil.add_weight(measure.fst, 1.05),
-                # pynutil.add_weight(time.fst, 1.1),
-                # pynutil.add_weight(whitelist.fst, 1.1),
+                pynutil.add_weight(time.fst, 1.1),
+                pynutil.add_weight(whitelist.fst, 1.1),
                 pynutil.add_weight(cardinal.fst, 3.06),
-                # pynutil.add_weight(math_symbol.fst, 3.08),
                 pynutil.add_weight(decimal.fst, 3.05),
-                # pynutil.add_weight(ordinal.fst, 1.1),
+                pynutil.add_weight(ordinal.fst, 1.1),
                 pynutil.add_weight(word.fst, 300),
             )
             token = pynutil.insert("tokens { ") + classify + pynutil.insert(" } ")
