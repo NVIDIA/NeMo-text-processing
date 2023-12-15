b1db6c5e348d684d554ab3b9185d3d77665a897b
Alex Cui <alexcui1994@gmail.com>
2023-12-15 09:03:37 -0800

    removing unused files
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/zh/taggers/apply_fst.py
+++
@@ -1,8 +0,0 @@
-import pynini
-
-
-def apply_fst(text, fst):  # applies this function for test purposes: apply_fst("the text",the fst_you_built)
-    try:
-        print(pynini.shortestpath(text @ fst).string())
-    except:
-        print(f"Error: No valid output with given input: ' {text}'")
