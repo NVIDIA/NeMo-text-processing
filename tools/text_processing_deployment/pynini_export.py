1c58605066d9ead04bfa04a2b0470106f99046d0
Chinmay Patil <chinmaypatil2000@gmail.com>
2024-01-02 18:36:52 +0530

    Added Marathi ITN
    Signed-off-by: Chinmay Patil <chinmaypatil2000@gmail.com>

--- tools/text_processing_deployment/pynini_export.py
+++ tools/text_processing_deployment/pynini_export.py
@@ -79,7 +79,7 @@ def parse_args():
     parser.add_argument(
         "--language",
         help="language",
-        choices=["en", "de", "es", "pt", "ru", 'fr', 'hu', 'sv', 'vi', 'zh', 'ar', 'it', 'es_en'],
+        choices=["en", "de", "es", "pt", "ru", 'fr', 'hu', 'sv', 'vi', 'zh', 'ar', 'it', 'es_en', 'mr'],
         type=str,
         default='en',
     )
@@ -110,7 +110,7 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    if args.language in ['pt', 'ru', 'vi', 'es_en'] and args.grammars == 'tn_grammars':
+    if args.language in ['pt', 'ru', 'vi', 'es_en', 'mr'] and args.grammars == 'tn_grammars':
         raise ValueError('Only ITN grammars could be deployed in Sparrowhawk for the selected languages.')
 
     if args.language == 'en':
@@ -225,6 +225,13 @@ if __name__ == '__main__':
             ClassifyFst as ITNClassifyFst,
         )
         from nemo_text_processing.inverse_text_normalization.es_en.verbalizers.verbalize import (
+            VerbalizeFst as ITNVerbalizeFst,
+        )
+    elif args.language == 'mr':
+        from nemo_text_processing.inverse_text_normalization.mr.taggers.tokenize_and_classify import (
+            ClassifyFst as ITNClassifyFst,
+        )
+        from nemo_text_processing.inverse_text_normalization.mr.verbalizers.verbalize import (
             VerbalizeFst as ITNVerbalizeFst,
         )
     output_dir = os.path.join(args.output_dir, args.language)
