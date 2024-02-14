4f9da16f6f965bb42e8bdf7a450ffde3d61386e8
Evelina <10428420+ekmb@users.noreply.github.com>
2024-02-14 14:28:26 -0800

    update isort - fix precommit (#138)
    * update isort version
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * update isort version
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * fix format
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove unused imports
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Evelina <ebakhturina@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/mr/test_cardinal.py
+++ tests/nemo_text_processing/mr/test_cardinal.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,17 +15,17 @@
 import pytest
 from parameterized import parameterized
 
-from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
+from nemo_text_processing.text_normalization.normalize import Normalizer
 
 from ..utils import CACHE_DIR, parse_test_case_file
 
 
-class TestCardinal:
-    inverse_normalizer_mr = InverseNormalizer(lang='mr', cache_dir=CACHE_DIR, overwrite_cache=False)
+class TestPreprocess:
+    normalizer_zh = Normalizer(lang='zh', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
 
-    @parameterized.expand(parse_test_case_file('mr/data_inverse_text_normalization/test_cases_cardinal.txt'))
+    @parameterized.expand(parse_test_case_file('zh/data_text_normalization/test_cases_preprocess.txt'))
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
-    def test_denorm(self, test_input, expected):
-        pred = self.inverse_normalizer_mr.inverse_normalize(test_input, verbose=False)
-        assert pred == expected
+    def test_norm_preprocess(self, test_input, expected):
+        preds = self.normalizer_zh.normalize(test_input)
+        assert expected == preds
