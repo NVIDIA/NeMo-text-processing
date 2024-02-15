02fae02b2b22ee3a58e7da9bd784995b3906ea79
David Sargsyan <66821320+davidks13@users.noreply.github.com>
2024-02-15 22:44:37 +0400

    Armenian itn (#136)
    * Added Armenian ITN
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * Added Armenian ITN
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * Added Armenian ITN
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * Added context for tests and fixed CodeQL errors
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * Revert "Added context for tests and fixed CodeQL errors"
    
    This reverts commit 2c804d941963c0be21d3aad07e6cd13568ab747b.
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * Added context to some test files and fixed CodeQL errors
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * deleted unnecessary data
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * translated a few measurements to Armenian
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * adjusted some things for better readability and maintainer support
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixed one test case and some issues
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: David Sargsyan <d.sargsyan@ispras.ru>
    Co-authored-by: David Sargsyan <d.sargsyan@ispras.ru>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/hy/test_measure.py
+++ tests/nemo_text_processing/hy/test_measure.py
@@ -22,7 +22,7 @@ from ..utils import CACHE_DIR, parse_test_case_file
 
 
 class TestMeasure:
-
+    
     inverse_normalizer = InverseNormalizer(lang='hy', cache_dir=CACHE_DIR, overwrite_cache=False)
 
     @parameterized.expand(parse_test_case_file('hy/data_inverse_text_normalization/test_cases_measure.txt'))
