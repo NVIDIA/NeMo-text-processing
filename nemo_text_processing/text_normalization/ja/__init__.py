b06e0ca1ed7c2762df36e15379e8d5af64539427
Alex Cui <alexcui1994@gmail.com>
2024-02-23 11:36:51 -0800

    format license fixings
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/ja/__init__.py
+++ nemo_text_processing/text_normalization/ja/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_text_processing.inverse_text_normalization.ja.taggers.tokenize_and_classify import ClassifyFst
-from nemo_text_processing.inverse_text_normalization.ja.verbalizers.verbalize import VerbalizeFst
-from nemo_text_processing.inverse_text_normalization.ja.verbalizers.verbalize_final import VerbalizeFinalFst
+
+from nemo_text_processing.text_normalization.ja.taggers.tokenize_and_classify import ClassifyFst
+from nemo_text_processing.text_normalization.ja.verbalizers.verbalize import VerbalizeFst
+from nemo_text_processing.text_normalization.ja.verbalizers.verbalize_final import VerbalizeFinalFst
--- nemo_text_processing/text_normalization/ja/taggers/word.py
+++ nemo_text_processing/text_normalization/ja/taggers/word.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2024 NVIDIA CORPORATION.  All rights reserved.
 # Copyright 2015 and onwards Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 import pynini
-from nemo_text_processing.text_normalization.jp.graph_utils import NEMO_NOT_SPACE, GraphFst
+from nemo_text_processing.text_normalization.ja.graph_utils import NEMO_NOT_SPACE, GraphFst
 from pynini.lib import pynutil
 
 
