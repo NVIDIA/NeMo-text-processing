48928334ddace824281e17cb44f475412e497237
Alex Cui <alexcui1994@gmail.com>
2024-02-23 09:09:58 -0800

    adding fiels
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/ja/graph_utils.py
+++ nemo_text_processing/text_normalization/ja/graph_utils.py
@@ -73,13 +73,6 @@ NEMO_LOWER_NOT_A = pynini.union(
     "y",
     "z",
 ).optimize()
-NEMO_SPACES_AND_ALHPANUMERICS = pynini.closure(
-    NEMO_SIGMA
-    | pynini.closure(NEMO_SPACE)
-    | pynini.closure(NEMO_NON_BREAKING_SPACE)
-    | pynini.closure(NEMO_DIGIT)
-    | pynini.closure(NEMO_WHITE_SPACE)
-).optimize()
 
 delete_space = pynutil.delete(pynini.closure(NEMO_WHITE_SPACE))
 delete_zero_or_one_space = pynutil.delete(pynini.closure(NEMO_WHITE_SPACE, 0, 1))
