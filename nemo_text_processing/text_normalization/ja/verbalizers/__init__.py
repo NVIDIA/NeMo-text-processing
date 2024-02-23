2d320193c5ff75045b7aef18514e61d0f1d43493
Alex Cui <alexcui1994@gmail.com>
2024-02-23 09:11:03 -0800

    verbalizers for numebrs
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/ja/verbalizers/__init__.py
+++ nemo_text_processing/text_normalization/ja/verbalizers/__init__.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+=======
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+>>>>>>> 68ffce7e (verbalizers for numebrs)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,3 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+<<<<<<< HEAD
+=======
+
+>>>>>>> 68ffce7e (verbalizers for numebrs)
