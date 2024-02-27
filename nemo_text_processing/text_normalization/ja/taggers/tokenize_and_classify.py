54bd206746fb5db33be3e64e0815862eec425ca5
Alex Cui <alexcui1994@gmail.com>
2024-02-27 15:39:33 -0800

    updates for space removal
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/ja/taggers/tokenize_and_classify.py
+++ nemo_text_processing/text_normalization/ja/taggers/tokenize_and_classify.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
-# Copyright 2015 and onwards Google, Inc.
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,37 +13,35 @@
 # limitations under the License.
 
 
-import logging
 import os
 
 import pynini
-from nemo_text_processing.text_normalization.ja.graph_utils import (
-    INPUT_LOWER_CASED,
-    GraphFst,
-    delete_extra_space,
-    delete_space,
-    generator_main,
-)
+from pynini.lib import pynutil
+
+from nemo_text_processing.text_normalization.ja.graph_utils import NEMO_SIGMA, GraphFst
 from nemo_text_processing.text_normalization.ja.taggers.cardinal import CardinalFst
-#from nemo_text_processing.text_normalization.ja.taggers.date import DateFst
-#from nemo_text_processing.text_normalization.ja.taggers.decimal import DecimalFst
-#from nemo_text_processing.text_normalization.ja.taggers.fraction import FractionFst
-#from nemo_text_processing.text_normalization.ja.taggers.ordinal import OrdinalFst
-from nemo_text_processing.text_normalization.ja.taggers.punctuation import PunctuationFst
-#from nemo_text_processing.text_normalization.ja.taggers.time import TimeFst
+# from nemo_text_processing.text_normalization.zh.taggers.date import DateFst
+# from nemo_text_processing.text_normalization.zh.taggers.decimal import DecimalFst
+# from nemo_text_processing.text_normalization.zh.taggers.fraction import FractionFst
+# from nemo_text_processing.text_normalization.zh.taggers.measure import MeasureFst
+# from nemo_text_processing.text_normalization.zh.taggers.money import MoneyFst
+# from nemo_text_processing.text_normalization.zh.taggers.ordinal import OrdinalFst
+#from nemo_text_processing.text_normalization.zh.taggers.preprocessor import PreProcessorFst
+# from nemo_text_processing.text_normalization.zh.taggers.time import TimeFst
 from nemo_text_processing.text_normalization.ja.taggers.whitelist import WhiteListFst
 from nemo_text_processing.text_normalization.ja.taggers.word import WordFst
-from pynini.lib import pynutil
 
 
 class ClassifyFst(GraphFst):
     """
-    Final class that composes all other classification grammars. This class can process an entire sentence, that is lower cased.
-    For deployment, this grammar will be compiled and exported to OpenFst Finite State Archive (FAR) File.
+    Final class that composes all other classification grammars. This class can process an entire sentence including punctuation.
+    For deployment, this grammar will be compiled and exported to OpenFst Finate State Archiv (FAR) File. 
     More details to deployment at NeMo/tools/text_processing_deployment.
-
+    
     Args:
         input_case: accepting either "lower_cased" or "cased" input.
+        deterministic: if True will provide a single transduction option,
+            for False multiple options (used for audio-based normalization)
         cache_dir: path to a dir with .far grammar file. Set to None to avoid using cache.
         overwrite_cache: set to True to overwrite .far files
         whitelist: path to a file with whitelist replacements
@@ -52,68 +49,47 @@ class ClassifyFst(GraphFst):
 
     def __init__(
         self,
-        input_case: str = INPUT_LOWER_CASED,
+        input_case: str,
         deterministic: bool = True,
         cache_dir: str = None,
         overwrite_cache: bool = False,
         whitelist: str = None,
     ):
-        super().__init__(name="tokenize_and_classify", kind="classify")
+        super().__init__(name="tokenize_and_classify", kind="classify", deterministic=deterministic)
 
         far_file = None
         if cache_dir is not None and cache_dir != "None":
             os.makedirs(cache_dir, exist_ok=True)
-            far_file = os.path.join(cache_dir, f"jp_tn_{input_case}.far")
+            whitelist_file = os.path.basename(whitelist) if whitelist else ""
+            far_file = os.path.join(cache_dir, f"zh_tn_{deterministic}_deterministic_{whitelist_file}_tokenize.far")
         if not overwrite_cache and far_file and os.path.exists(far_file):
             self.fst = pynini.Far(far_file, mode="r")["tokenize_and_classify"]
-            logging.info(f"ClassifyFst.fst was restored from {far_file}.")
         else:
-            logging.info(f"Creating ClassifyFst grammars.")
-            cardinal = CardinalFst()
-            cardinal_graph = cardinal.fst
+            #date = DateFst(deterministic=deterministic)
+            cardinal = CardinalFst(deterministic=deterministic)
+            #decimal = DecimalFst(cardinal=cardinal, deterministic=deterministic)
+            word = WordFst(deterministic=deterministic)
+            #fraction = FractionFst(cardinal=cardinal, deterministic=deterministic)
+            #money = MoneyFst(cardinal=cardinal, deterministic=deterministic)
+            #measure = MeasureFst(cardinal=cardinal, decimal=decimal, fraction=fraction, deterministic=deterministic)
+            #time = TimeFst(deterministic=deterministic)
+            whitelist = WhiteListFst(deterministic=deterministic)
+            #ordinal = OrdinalFst(cardinal=cardinal, deterministic=deterministic)
 
-            #ordinal = OrdinalFst(cardinal)
-            #ordinal_graph = ordinal.fst
-
-            #date = DateFst(cardinal)
-            #date_graph = date.fst
-
-            #decimal = DecimalFst(cardinal)
-            #decimal_graph = decimal.fst
-
-            #fraction = FractionFst(cardinal, decimal)
-            #fraction_graph = fraction.fst
-
-            #time = TimeFst(cardinal)
-            #time_graph = time.fst
-
-            word_graph = WordFst().fst
-
-            whitelist_graph = WhiteListFst().fst
-            punct_graph = PunctuationFst().fst
-
-            classify = (
-                pynutil.add_weight(whitelist_graph, 1.01)
-                | pynutil.add_weight(cardinal_graph, -1.1)
-                #| pynutil.add_weight(ordinal_graph, 1.1)
-                #| pynutil.add_weight(date_graph, 1.1)
-                #| pynutil.add_weight(decimal_graph, 1.1)
-                #| pynutil.add_weight(fraction_graph, 1.1)
-                #| pynutil.add_weight(time_graph, 1.1)
-                | pynutil.add_weight(word_graph, 100)
+            classify = pynini.union(
+                #pynutil.add_weight(date.fst, 1.1),
+                #pynutil.add_weight(fraction.fst, 1.0),
+                #pynutil.add_weight(money.fst, 1.1),
+                #pynutil.add_weight(measure.fst, 1.05),
+                #pynutil.add_weight(time.fst, 1.1),
+                pynutil.add_weight(whitelist.fst, 1.1),
+                pynutil.add_weight(cardinal.fst, 1.1),
+                #pynutil.add_weight(decimal.fst, 3.05),
+                #pynutil.add_weight(ordinal.fst, 1.1),
+                pynutil.add_weight(word.fst, 100),
             )
 
-            punct = pynutil.insert("tokens { ") + pynutil.add_weight(punct_graph, weight=1.1) + pynutil.insert(" }")
-            token = pynutil.insert("tokens { ") + classify + pynutil.insert(" }")
-            token_plus_punct = (
-                pynini.closure(punct + pynutil.insert(" ")) + token + pynini.closure(pynutil.insert(" ") + punct)
-            )
+            token = pynutil.insert("tokens { ") + classify + pynutil.insert(" } ")
+            tagger = pynini.cdrewrite(token.optimize(), "", "", NEMO_SIGMA).optimize()
 
-            graph = token_plus_punct + pynini.closure(delete_extra_space + token_plus_punct)
-            graph = delete_space + graph + delete_space
-
-            self.fst = graph.optimize()
-
-            if far_file:
-                generator_main(far_file, {"tokenize_and_classify": self.fst})
-                logging.info(f"ClassifyFst grammars are saved to {far_file}.")
+            self.fst = tagger
