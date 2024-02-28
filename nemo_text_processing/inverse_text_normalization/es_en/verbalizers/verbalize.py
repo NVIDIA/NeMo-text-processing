5fcafaeff1244d3e867c8a3320e35d84e46b54e9
Mariana Graterol Fuenmayor <mgrafu@gmail.com>
2024-02-28 13:17:40 -0800

    enable capitalized itn for es_en
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>

--- nemo_text_processing/inverse_text_normalization/es_en/verbalizers/verbalize.py
+++ nemo_text_processing/inverse_text_normalization/es_en/verbalizers/verbalize.py
@@ -74,10 +74,12 @@ class VerbalizeFst(GraphFst):
         en_date_graph = EnDateFst().fst
         en_whitelist_graph = EnWhiteListFst().fst
         en_telephone_graph = EnTelephoneFst().fst
+        en_time_graph = EnTimeFst().fst
         en_electronic_graph = EnElectronicFst().fst
 
         graph = (
-            time_graph
+            en_time_graph
+            | pynutil.add_weight(time_graph, 1.1)
             | date_graph
             | pynutil.add_weight(en_date_graph, 1.1)
             | money_graph
