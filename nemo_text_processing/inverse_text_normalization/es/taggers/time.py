1ea5fd5ac7e12cf51400f7406fa36c0249c3d69d
Mariana Graterol Fuenmayor <mgrafu@gmail.com>
2024-03-26 08:35:58 -0700

    fix imports
    Signed-off-by: Mariana Graterol Fuenmayor <mgrafu@gmail.com>

--- nemo_text_processing/inverse_text_normalization/es/taggers/time.py
+++ nemo_text_processing/inverse_text_normalization/es/taggers/time.py
@@ -14,6 +14,7 @@
 
 
 import pynini
+from pynini.lib import pynutil
 from nemo_text_processing.inverse_text_normalization.es.utils import get_abs_path
 from nemo_text_processing.text_normalization.en.graph_utils import (
     INPUT_CASED,
