9e2b2887d8ee6588180158d86a4f14370380c372
Alex Cui <alexcui1994@gmail.com>
2024-04-01 15:12:23 -0700

    removing comments
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/zh/taggers/tokenize_and_classify.py
+++ nemo_text_processing/text_normalization/zh/taggers/tokenize_and_classify.py
@@ -65,22 +65,22 @@ class ClassifyFst(GraphFst):
         if not overwrite_cache and far_file and os.path.exists(far_file):
             self.fst = pynini.Far(far_file, mode="r")["tokenize_and_classify"]
         else:
-            date = DateFst(deterministic=deterministic)
             cardinal = CardinalFst(deterministic=deterministic)
+            date = DateFst(deterministic=deterministic)
             decimal = DecimalFst(cardinal=cardinal, deterministic=deterministic)
-            word = WordFst(deterministic=deterministic)
+            time = TimeFst(deterministic=deterministic)
             fraction = FractionFst(cardinal=cardinal, deterministic=deterministic)
             money = MoneyFst(cardinal=cardinal, deterministic=deterministic)
-            #measure = MeasureFst(cardinal=cardinal, decimal=decimal, fraction=fraction, deterministic=deterministic)
-            time = TimeFst(deterministic=deterministic)
-            whitelist = WhiteListFst(deterministic=deterministic)
+            measure = MeasureFst(cardinal=cardinal, decimal=decimal, fraction=fraction, deterministic=deterministic)
             ordinal = OrdinalFst(cardinal=cardinal, deterministic=deterministic)
-
+            whitelist = WhiteListFst(deterministic=deterministic)
+            word = WordFst(deterministic=deterministic)
+          
             classify = pynini.union(
                 pynutil.add_weight(date.fst, 1.1),
-                pynutil.add_weight(fraction.fst, 1.0),
+                pynutil.add_weight(fraction.fst, 1.0), # was 1.0
                 pynutil.add_weight(money.fst, 1.1),
-                #pynutil.add_weight(measure.fst, 1.05),
+                pynutil.add_weight(measure.fst, 1.05),
                 pynutil.add_weight(time.fst, 1.1),
                 pynutil.add_weight(whitelist.fst, 1.1),
                 pynutil.add_weight(cardinal.fst, 1.1),
@@ -91,5 +91,5 @@ class ClassifyFst(GraphFst):
 
             token = pynutil.insert("tokens { ") + classify + pynutil.insert(" } ")
             tagger = pynini.cdrewrite(token.optimize(), "", "", NEMO_SIGMA).optimize()
-
+      
             self.fst = tagger
