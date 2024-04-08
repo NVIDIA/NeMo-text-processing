fe930e4a663031ae28c1af9c30542ff9898b91bb
Alex Cui <alexcui1994@gmail.com>
2024-04-08 11:42:51 -0700

    updates for reducing testing time
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/zh/taggers/tokenize_and_classify.py
+++ nemo_text_processing/text_normalization/zh/taggers/tokenize_and_classify.py
@@ -18,6 +18,8 @@ import os
 import pynini
 from pynini.lib import pynutil
 
+from nemo_text_processing.text_normalization.zh.graph_utils import generator_main
+
 from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_SIGMA, GraphFst
 from nemo_text_processing.text_normalization.zh.taggers.cardinal import CardinalFst
 from nemo_text_processing.text_normalization.zh.taggers.date import DateFst
@@ -90,7 +92,10 @@ class ClassifyFst(GraphFst):
             )
 
             token = pynutil.insert("tokens { ") + classify + pynutil.insert(" } ")
-            tagger = pynini.cdrewrite(token.optimize(), "", "", NEMO_SIGMA).optimize()
+            # added line 94
+            tagger = pynini.closure(token,1) 
+            # original line that worked for resolving sentence inputs: 
+            ##tagger = pynini.cdrewrite(token.optimize(), "", "", NEMO_SIGMA).optimize()
 
             self.fst = tagger
 
