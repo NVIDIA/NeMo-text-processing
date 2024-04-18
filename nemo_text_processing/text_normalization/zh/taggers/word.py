396b2122bfdd014709563bcf86f5f25aafe1d8b0
Buyuan(Alex) Cui <69030297+BuyuanCui@users.noreply.github.com>
2024-04-18 14:47:47 -0700

    Update word.py
    Fixing code style, removing unused imports
    
    Signed-off-by: Buyuan(Alex) Cui <69030297+BuyuanCui@users.noreply.github.com>

--- nemo_text_processing/text_normalization/zh/taggers/word.py
+++ nemo_text_processing/text_normalization/zh/taggers/word.py
@@ -16,7 +16,7 @@
 import pynini
 from pynini.lib import pynutil
 
-from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_CHAR, NEMO_NOT_QUOTE, NEMO_NOT_SPACE, GraphFst
+from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_NOT_QUOTE, GraphFst
 
 
 class WordFst(GraphFst):
