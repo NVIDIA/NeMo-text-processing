b227b0739f64bec932b2a883da20cf747e617ff1
Alex Cui <alexcui1994@gmail.com>
2024-05-15 14:50:48 -0700

    copied from main
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tools/text_processing_deployment/pynini_export.py
+++ tools/text_processing_deployment/pynini_export.py
@@ -260,6 +260,7 @@ if __name__ == '__main__':
         from nemo_text_processing.inverse_text_normalization.ja.verbalizers.verbalize import (
             VerbalizeFst as ITNVerbalizeFst,
         )
+        from nemo_text_processing.inverse_text_normalization.ja.verbalizers.post_processing import (PostProcessingFst as TNPostProcessingFst)
     output_dir = os.path.join(args.output_dir, f"{args.language}_{args.grammars}_{args.input_case}")
     export_grammars(
         output_dir=output_dir,
