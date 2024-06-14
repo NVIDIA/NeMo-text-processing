f35361b88528a9b6b687d19f9643db51c9339cf2
Simon Zuberek <szuberek@nvidia.com>
2024-06-14 14:22:36 -0400

    Fixes for #166
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>

--- nemo_text_processing/text_normalization/it/taggers/electronic.py
+++ nemo_text_processing/text_normalization/it/taggers/electronic.py
@@ -15,11 +15,15 @@
 import pynini
 from pynini.lib import pynutil
 
-from nemo_text_processing.text_normalization.en.graph_utils import NEMO_ALPHA, NEMO_DIGIT, GraphFst, insert_space
+from nemo_text_processing.text_normalization.en.graph_utils import (
+    NEMO_ALPHA,
+    NEMO_DIGIT,
+    GraphFst,
+    insert_space,
+)
 from nemo_text_processing.text_normalization.it.utils import get_abs_path, load_labels
 
-common_domains = [x[0] for x in load_labels(get_abs_path("data/electronic/domain.tsv"))]
-symbols = [x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))]
+# common_domains = [x[0] for x in load_labels(get_abs_path("data/electronic/domain.tsv"))]
 
 
 class ElectronicFst(GraphFst):
@@ -34,48 +38,62 @@ class ElectronicFst(GraphFst):
     """
 
     def __init__(self, deterministic: bool = True):
-        super().__init__(name="electronic", kind="classify", deterministic=deterministic)
+        super().__init__(
+            name="electronic", kind="classify", deterministic=deterministic
+        )
 
         dot = pynini.accep(".")
-        accepted_common_domains = pynini.union(*common_domains)
-        accepted_symbols = pynini.union(*symbols) - dot
-        accepted_characters = pynini.closure(NEMO_ALPHA | NEMO_DIGIT | accepted_symbols)
-        acceepted_characters_with_dot = pynini.closure(NEMO_ALPHA | NEMO_DIGIT | accepted_symbols | dot)
 
-        # e-mail
-        username = (
-            pynutil.insert("username: \"")
-            + acceepted_characters_with_dot
-            + pynutil.insert("\"")
-            + pynini.cross('@', ' ')
+        symbols = [
+            x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))
+        ]
+        symbols = pynini.union(*symbols)
+        symbols_no_period = pynini.difference(symbols, dot)
+        accepted_characters = pynini.closure(
+            (NEMO_ALPHA | NEMO_DIGIT | symbols_no_period), 1
         )
+        all_characters = pynini.closure((NEMO_ALPHA | NEMO_DIGIT | symbols), 1)
 
-        domain_graph = accepted_characters + dot + accepted_characters
+        # domains
+        domain = dot + accepted_characters
         domain_graph = (
-            pynutil.insert("domain: \"")
-            + domain_graph
-            + pynini.closure((accepted_symbols | dot) + pynini.closure(accepted_characters, 1), 0, 1)
-            + pynutil.insert("\"")
+            pynutil.insert('domain: "')
+            + (accepted_characters + pynini.closure(domain, 1))
+            + pynutil.insert('"')
         )
 
-        domain_common_graph = (
-            pynutil.insert("domain: \"")
-            + accepted_characters
-            + accepted_common_domains
-            + pynini.closure((accepted_symbols | dot) + pynini.closure(accepted_characters, 1), 0, 1)
-            + pynutil.insert("\"")
+        # email
+        username = (
+            pynutil.insert('username: "')
+            + all_characters
+            + pynutil.insert('"')
+            + pynini.cross("@", " ")
+        )
+        email = username + domain_graph
+
+        # social media tags
+        tag = (
+            pynini.cross("@", "")
+            + pynutil.insert('username: "')
+            + (accepted_characters | (accepted_characters + pynini.closure(domain, 1)))
+            + pynutil.insert('"')
         )
 
-        graph = (username + domain_graph) | domain_common_graph
-
+        # url
         protocol_start = pynini.accep("https://") | pynini.accep("http://")
         protocol_end = (
-            pynini.accep("www.") if deterministic else pynini.accep("www.") | pynini.cross("www.", "vu vu vu.")
+            pynini.accep("www.")
+            if deterministic
+            else pynini.accep("www.") | pynini.cross("www.", "vu vu vu.")
         )
         protocol = protocol_start | protocol_end | (protocol_start + protocol_end)
-        protocol = pynutil.insert("protocol: \"") + protocol + pynutil.insert("\"")
-        graph |= protocol + insert_space + (domain_graph | domain_common_graph)
+        protocol = pynutil.insert('protocol: "') + protocol + pynutil.insert('"')
+        url = protocol + insert_space + (domain_graph)
+
+        graph = url | domain_graph | email | tag
         self.graph = graph
 
-        final_graph = self.add_tokens(self.graph + pynutil.insert(" preserve_order: true"))
+        final_graph = self.add_tokens(
+            self.graph + pynutil.insert(" preserve_order: true")
+        )
         self.fst = final_graph.optimize()
--- nemo_text_processing/text_normalization/it/verbalizers/electronic.py
+++ nemo_text_processing/text_normalization/it/verbalizers/electronic.py
@@ -18,13 +18,16 @@ from pynini.lib import pynutil
 from nemo_text_processing.text_normalization.en.graph_utils import (
     NEMO_NOT_QUOTE,
     NEMO_SIGMA,
+    NEMO_SPACE,
     GraphFst,
     delete_preserve_order,
     insert_space,
 )
 from nemo_text_processing.text_normalization.it.utils import get_abs_path
 
-digit_no_zero = pynini.invert(pynini.string_file(get_abs_path("data/numbers/digit.tsv")))
+digit_no_zero = pynini.invert(
+    pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
+)
 zero = pynini.invert(pynini.string_file(get_abs_path("data/numbers/zero.tsv")))
 
 graph_symbols = pynini.string_file(get_abs_path("data/electronic/symbols.tsv"))
@@ -35,7 +38,7 @@ domain_common = pynini.string_file(get_abs_path("data/electronic/domain.tsv"))
 class ElectronicFst(GraphFst):
     """
     Finite state transducer for verbalizing electronic
-        e.g. electronic { username: "abc.def2" domain: "studenti.università.it" } -> 
+        e.g. electronic { username: "abc.def2" domain: "studenti.università.it" } ->
         "a b c punto d e f due chiocciola s t u d e n t i punto u n i v e r s i t à punto IT
     Args:
         deterministic: if True will provide a single transduction option,
@@ -43,7 +46,9 @@ class ElectronicFst(GraphFst):
     """
 
     def __init__(self, deterministic: bool = True):
-        super().__init__(name="electronic", kind="verbalize", deterministic=deterministic)
+        super().__init__(
+            name="electronic", kind="verbalize", deterministic=deterministic
+        )
 
         graph_digit = digit_no_zero | zero
 
@@ -52,23 +57,35 @@ class ElectronicFst(GraphFst):
                 NEMO_NOT_QUOTE - pynini.accep(" ")
             )
 
-        verbalize_characters = pynini.cdrewrite(graph_symbols | graph_digit, "", "", NEMO_SIGMA)
+        verbalize_characters = pynini.cdrewrite(
+            graph_symbols | graph_digit, "", "", NEMO_SIGMA
+        )
 
-        user_name = pynutil.delete("username: \"") + add_space_after_char() + pynutil.delete("\"")
+        user_name = (
+            pynutil.delete('username: "') + add_space_after_char() + pynutil.delete('"')
+        )
         user_name @= verbalize_characters
 
-        convert_defaults = pynutil.add_weight(NEMO_NOT_QUOTE, weight=0.0001) | server_common | domain_common
+        convert_defaults = (
+            pynutil.add_weight(NEMO_NOT_QUOTE, weight=0.0001)
+            | server_common
+            | domain_common
+        )
         domain = convert_defaults + pynini.closure(insert_space + convert_defaults)
         domain @= verbalize_characters
 
-        domain = pynutil.delete("domain: \"") + domain + pynutil.delete("\"")
+        domain = pynutil.delete('domain: "') + domain + pynutil.delete('"')
         protocol = (
-            pynutil.delete("protocol: \"")
-            + add_space_after_char() @ pynini.cdrewrite(graph_symbols, "", "", NEMO_SIGMA)
-            + pynutil.delete("\"")
+            pynutil.delete('protocol: "')
+            + add_space_after_char()
+            @ pynini.cdrewrite(graph_symbols, "", "", NEMO_SIGMA)
+            + pynutil.delete('"')
         )
-        self.graph = (pynini.closure(protocol + pynini.accep(" "), 0, 1) + domain) | (
-            user_name + pynini.accep(" ") + pynutil.insert("chiocciola ") + domain
+
+        self.graph = (pynini.closure(protocol + NEMO_SPACE, 0, 1) + domain) | (
+            user_name + NEMO_SPACE + pynutil.insert("chiocciola ") + domain
+            | (pynutil.insert("chiocciola ") + user_name)
         )
+
         delete_tokens = self.delete_tokens(self.graph + delete_preserve_order)
         self.fst = delete_tokens.optimize()
