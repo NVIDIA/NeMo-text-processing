aad151924abc27ee27bf1ed79e83ad41c8817b01
Simon Zuberek <szuberek@nvidia.com>
2024-07-16 16:27:10 -0400

    Fixes the period variable
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>

--- nemo_text_processing/text_normalization/hu/taggers/electronic.py
+++ nemo_text_processing/text_normalization/hu/taggers/electronic.py
@@ -48,14 +48,20 @@ class ElectronicFst(GraphFst):
     """
 
     def __init__(self, deterministic: bool = True):
-        super().__init__(name="electronic", kind="classify", deterministic=deterministic)
+        super().__init__(
+            name="electronic", kind="classify", deterministic=deterministic
+        )
 
-        dot = pynini.accep(period)
+        period_fst = pynini.accep(period)
 
-        symbols = [x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))]
+        symbols = [
+            x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))
+        ]
         symbols = pynini.union(*symbols)
         # all symbols
-        symbols_no_period = pynini.difference(symbols, dot)  # alphabet of accepted symbols excluding the '.'
+        symbols_no_period = pynini.difference(
+            symbols, period_fst
+        )  # alphabet of accepted symbols excluding the '.'
         accepted_characters = pynini.closure(
             (NEMO_ALPHA | NEMO_DIGIT | symbols_no_period), 1
         )  # alphabet of accepted chars excluding the '.'
@@ -64,7 +70,7 @@ class ElectronicFst(GraphFst):
         )  # alphabet of accepted chars including the '.'
 
         # domains
-        domain = dot + accepted_characters
+        domain = period_fst + accepted_characters
         domain_graph = (
             pynutil.insert(domain_string + colon + NEMO_SPACE + double_quotes)
             + (accepted_characters + pynini.closure(domain, 1))
@@ -89,7 +95,9 @@ class ElectronicFst(GraphFst):
         )
 
         # url
-        protocol_start = pynini.accep(https + colon + double_slash) | pynini.accep(http + colon + double_slash)
+        protocol_start = pynini.accep(https + colon + double_slash) | pynini.accep(
+            http + colon + double_slash
+        )
         protocol_end = (
             pynini.accep(www + period)
             if deterministic
@@ -111,123 +119,7 @@ class ElectronicFst(GraphFst):
         graph = url | domain_graph | email | tag
         self.graph = graph
 
-        final_graph = self.add_tokens(self.graph + pynutil.insert(" preserve_order: true"))
-        self.fst = final_graph.optimize()
-
-
-'''
-# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-import pynini
-from pynini.lib import pynutil
-
-from nemo_text_processing.text_normalization.de.utils import get_abs_path, load_labels
-from nemo_text_processing.text_normalization.en.graph_utils import (
-    NEMO_ALPHA,
-    NEMO_DIGIT,
-    NEMO_NOT_SPACE,
-    NEMO_SIGMA,
-    NEMO_UPPER,
-    TO_UPPER,
-    GraphFst,
-    get_abs_path,
-    insert_space,
-    period,
-)
-
-
-class ElectronicFst(GraphFst):
-    """
-    Finite state transducer for classifying electronic: email addresses
-        e.g. "abc@hotmail.com" -> electronic { username: "abc" domain: "hotmail.com" preserve_order: true }
-        e.g. "www.abc.com/123" -> electronic { protocol: "www." domain: "abc.com/123" preserve_order: true }
-
-    Args:
-        deterministic: if True will provide a single transduction option,
-            for False multiple transduction are generated (used for audio-based normalization)
-    """
-
-    def __init__(self, deterministic: bool = True):
-        super().__init__(
-            name="electronic", kind="classify", deterministic=deterministic
-        )
-
-        dot = pynini.accep(period)
-
-        symbols = [
-            x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))
-        ]
-        symbols = pynini.union(*symbols)
-        # all symbols
-        symbols_no_period = pynini.difference(
-            symbols, dot
-        )  # alphabet of accepted symbols excluding the '.'
-        accepted_characters = pynini.closure(
-            (NEMO_ALPHA | NEMO_DIGIT | symbols_no_period), 1
-        )  # alphabet of accepted chars excluding the '.'
-        all_characters = pynini.closure(
-            (NEMO_ALPHA | NEMO_DIGIT | symbols), 1
-        )  # alphabet of accepted chars including the '.'
-
-        # domains
-        domain = dot + accepted_characters
-        domain_graph = (
-            pynutil.insert()
-            + (accepted_characters + pynini.closure(domain, 1))
-            + insert_double_quotes
-        )
-
-        # email
-        username = (
-            insert_username
-            + all_characters
-            + insert_double_quotes
-            + pynini.cross("@", " ")
-        )
-        email = username + domain_graph
-
-        # social media tags
-        tag = (
-            pynutil.delete("@")
-            + insert_username
-            + (accepted_characters | (accepted_characters + pynini.closure(domain, 1)))
-            + insert_double_quotes
-        )
-
-        # url
-        protocol_start = pynini.accep("https://") | pynini.accep("http://")
-        protocol_end = (
-            pynini.accep("www.")
-            if deterministic
-            else (
-                pynini.accep("www.")
-                | pynini.cross("www.", "vé vé vé.")
-                | pynini.cross("www.", "dupla vé dupla vé dupla vé.")
-                | pynini.cross("www.", "kettős vé kettős vé kettős vé.")
-            )
-        )
-        protocol = protocol_start | protocol_end | (protocol_start + protocol_end)
-        protocol = insert_protocol + protocol + insert_double_quotes
-        url = protocol + insert_space + (domain_graph)
-
-        graph = url | domain_graph | email | tag
-        self.graph = graph
-
         final_graph = self.add_tokens(
             self.graph + pynutil.insert(" preserve_order: true")
         )
         self.fst = final_graph.optimize()
-'''
