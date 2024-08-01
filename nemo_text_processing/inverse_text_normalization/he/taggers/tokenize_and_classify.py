2464c4865f7192a525989ef93c208371bc6cf405
dankeinan1 <dan.keinan@verbit.ai>
2024-08-01 18:05:49 +0200

    Hebrew ITN - disabled ordinal fst when not used in date
    Signed-off-by: nir.raviv <nir.raviv@verbit.ai>

--- nemo_text_processing/inverse_text_normalization/he/taggers/tokenize_and_classify.py
+++ nemo_text_processing/inverse_text_normalization/he/taggers/tokenize_and_classify.py
@@ -71,11 +71,13 @@ class ClassifyFst(GraphFst):
                 | pynutil.add_weight(decimal_graph, 1.1)
                 | pynutil.add_weight(measure_graph, 1.1)
                 | pynutil.add_weight(cardinal_graph, 1.1)
-                | pynutil.add_weight(ordinal_graph, 1.09)
                 | pynutil.add_weight(word_graph, 100)
                 # | pynutil.add_weight(money_graph, 1.1)
                 # | pynutil.add_weight(telephone_graph, 1.1)
                 # | pynutil.add_weight(electronic_graph, 1.1)
+                # NOTE: we convert ordinals in Hebrew only if it is a part of a date!
+                # this is why it is commented out.
+                # | pynutil.add_weight(ordinal_graph, 1.09)
             )
 
             punct = pynutil.insert("tokens { ") + pynutil.add_weight(punct_graph, weight=1.1) + pynutil.insert(" }")
