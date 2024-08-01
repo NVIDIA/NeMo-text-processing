98cc91a1209f24fe542aa137a8f572f915928e7a
dankeinan1 <dan.keinan@verbit.ai>
2024-08-01 17:12:51 +0200

    Hebrew ITN - added bug fix to unrecognized prefixes
    Signed-off-by: nir.raviv <nir.raviv@verbit.ai>

--- nemo_text_processing/inverse_text_normalization/he/verbalizers/cardinal.py
+++ nemo_text_processing/inverse_text_normalization/he/verbalizers/cardinal.py
@@ -1,6 +1,8 @@
 import pynini
 from pynini.lib import pynutil
-from nemo_text_processing.inverse_text_normalization.he.graph_utils import NEMO_DIGIT, NEMO_ALPHA, GraphFst, delete_space
+from nemo_text_processing.inverse_text_normalization.he.graph_utils import (
+    NEMO_DIGIT, NEMO_ALPHA, GraphFst, delete_space
+)
 
 
 class CardinalFst(GraphFst):
