ff359798a29969eacc5eb7c070a20883514298ec
dankeinan1 <dan.keinan@verbit.ai>
2024-08-05 16:04:27 +0200

    Hebrew ITN - code review changes

--- nemo_text_processing/inverse_text_normalization/en/utils.py
+++ nemo_text_processing/inverse_text_normalization/en/utils.py
@@ -44,7 +44,6 @@ def get_abs_path(rel_path):
         
     Returns absolute path
     """
-    print(os.path.dirname(os.path.abspath(__file__)) + '/' + rel_path)
     return os.path.dirname(os.path.abspath(__file__)) + '/' + rel_path
 
 
