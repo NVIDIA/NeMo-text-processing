26e77e1df0eaf8ef8a2f860e57d185e205b2eb98
Simon Zuberek <szuberek@nvidia.com>
2024-08-06 12:36:17 -0400

    Passes Sparrowhawk and normalization.py but not pyTest
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>

--- nemo_text_processing/text_normalization/it/verbalizers/electronic.py
+++ nemo_text_processing/text_normalization/it/verbalizers/electronic.py
@@ -29,7 +29,9 @@ from nemo_text_processing.text_normalization.en.graph_utils import (  # Common s
 )
 from nemo_text_processing.text_normalization.it.utils import get_abs_path
 
-digit_no_zero = pynini.invert(pynini.string_file(get_abs_path("data/numbers/digit.tsv")))
+digit_no_zero = pynini.invert(
+    pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
+)
 zero = pynini.invert(pynini.string_file(get_abs_path("data/numbers/zero.tsv")))
 
 graph_symbols = pynini.string_file(get_abs_path("data/electronic/symbols.tsv"))
@@ -48,16 +50,20 @@ class ElectronicFst(GraphFst):
     """
 
     def __init__(self, deterministic: bool = True):
-        super().__init__(name="electronic", kind="verbalize", deterministic=deterministic)
+        super().__init__(
+            name="electronic", kind="verbalize", deterministic=deterministic
+        )
 
         graph_digit = digit_no_zero | zero
 
         def add_space_after_char():
-            return pynini.closure(NEMO_NOT_QUOTE - pynini.accep(NEMO_SPACE) + pynutil.insert(NEMO_SPACE)) + (
-                NEMO_NOT_QUOTE - pynini.accep(NEMO_SPACE)
-            )
+            return pynini.closure(
+                NEMO_NOT_QUOTE - pynini.accep(NEMO_SPACE) + pynutil.insert(NEMO_SPACE)
+            ) + (NEMO_NOT_QUOTE - pynini.accep(NEMO_SPACE))
 
-        verbalize_characters = pynini.cdrewrite(graph_symbols | graph_digit, "", "", NEMO_SIGMA)
+        verbalize_characters = pynini.cdrewrite(
+            graph_symbols | graph_digit, "", "", NEMO_SIGMA
+        )
 
         user_name = (
             pynutil.delete(username_string + colon + NEMO_SPACE + double_quotes)
@@ -66,16 +72,25 @@ class ElectronicFst(GraphFst):
         )
         user_name @= verbalize_characters
 
-        convert_defaults = pynutil.add_weight(NEMO_NOT_QUOTE, weight=0.0001) | server_common | domain_common
-        domain = convert_defaults + pynini.closure(pynutil.insert(NEMO_SPACE) + convert_defaults)
+        convert_defaults = (
+            pynutil.add_weight(NEMO_NOT_QUOTE, weight=0.0001)
+            | server_common
+            | domain_common
+        )
+        domain = convert_defaults + pynini.closure(
+            pynutil.insert(NEMO_SPACE) + convert_defaults
+        )
         domain @= verbalize_characters
 
         domain = (
-            pynutil.delete(domain_string + colon + NEMO_SPACE + double_quotes) + domain + pynutil.delete(double_quotes)
+            pynutil.delete(domain_string + colon + NEMO_SPACE + double_quotes)
+            + domain
+            + pynutil.delete(double_quotes)
         )
         protocol = (
             pynutil.delete(protocol_string + colon + NEMO_SPACE + double_quotes)
-            + add_space_after_char() @ pynini.cdrewrite(graph_symbols, "", "", NEMO_SIGMA)
+            + add_space_after_char()
+            @ pynini.cdrewrite(graph_symbols, "", "", NEMO_SIGMA)
             + pynutil.delete(double_quotes)
         )
 
