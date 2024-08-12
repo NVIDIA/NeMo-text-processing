30f581ffeaf27887f4e2c69a2f2103f4a4dec69f
dankeinan1 <dan.keinan@verbit.ai>
2024-08-12 13:01:26 +0200

    Hebrew ITN - CR changes

--- nemo_text_processing/inverse_text_normalization/he/verbalizers/measure.py
+++ nemo_text_processing/inverse_text_normalization/he/verbalizers/measure.py
@@ -112,4 +112,4 @@ if __name__ == "__main__":
     g = MeasureFst(decimal, cardinal).fst
 
     # To test this FST, remove comment out and change the input text
-    # apply_fst("טקסט לבדיקה", g)
+    # apply_fst('טקסט לבדיקה כאן', g)
