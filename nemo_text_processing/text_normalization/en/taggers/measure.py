89ae5d0e2617442b7dadfe96d60ee3b282041f4c
Simon Zuberek <simon@zuberek.net>
2024-08-22 11:46:52 -0400

    EN TN Fixes for nv-bug 4786225 (#218)
    * Implements a workaround for sentences with addresses ending in terminal period abbreviations
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Updates the cache
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    Co-authored-by: Simon Zuberek <szuberek@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo_text_processing/text_normalization/en/taggers/measure.py
+++ nemo_text_processing/text_normalization/en/taggers/measure.py
@@ -39,7 +39,7 @@ from nemo_text_processing.text_normalization.en.verbalizers.ordinal import Ordin
 
 class MeasureFst(GraphFst):
     """
-    Finite state transducer for classifying measure, suppletive aware, e.g. 
+    Finite state transducer for classifying measure, suppletive aware, e.g.
         -12kg -> measure { negative: "true" cardinal { integer: "twelve" } units: "kilograms" }
         1kg -> measure { cardinal { integer: "one" } units: "kilogram" }
         .5kg -> measure { decimal { fractional_part: "five" } units: "kilograms" }
@@ -52,7 +52,9 @@ class MeasureFst(GraphFst):
             for False multiple transduction are generated (used for audio-based normalization)
     """
 
-    def __init__(self, cardinal: GraphFst, decimal: GraphFst, fraction: GraphFst, deterministic: bool = True):
+    def __init__(
+        self, cardinal: GraphFst, decimal: GraphFst, fraction: GraphFst, deterministic: bool = True,
+    ):
         super().__init__(name="measure", kind="classify", deterministic=deterministic)
         cardinal_graph = cardinal.graph_with_and | self.get_range(cardinal.graph_with_and)
 
@@ -61,13 +63,13 @@ class MeasureFst(GraphFst):
             graph_unit |= pynini.string_file(get_abs_path("data/measure/unit_alternatives.tsv"))
 
         graph_unit |= pynini.compose(
-            pynini.closure(TO_LOWER, 1) + (NEMO_ALPHA | TO_LOWER) + pynini.closure(NEMO_ALPHA | TO_LOWER), graph_unit
+            pynini.closure(TO_LOWER, 1) + (NEMO_ALPHA | TO_LOWER) + pynini.closure(NEMO_ALPHA | TO_LOWER), graph_unit,
         ).optimize()
 
         graph_unit_plural = convert_space(graph_unit @ SINGULAR_TO_PLURAL)
         graph_unit = convert_space(graph_unit)
 
-        optional_graph_negative = pynini.closure(pynutil.insert("negative: ") + pynini.cross("-", "\"true\" "), 0, 1)
+        optional_graph_negative = pynini.closure(pynutil.insert("negative: ") + pynini.cross("-", '"true" '), 0, 1)
 
         graph_unit2 = (
             pynini.cross("/", "per") + delete_zero_or_one_space + pynutil.insert(NEMO_NON_BREAKING_SPACE) + graph_unit
@@ -78,13 +80,11 @@ class MeasureFst(GraphFst):
         )
 
         unit_plural = (
-            pynutil.insert("units: \"")
-            + (graph_unit_plural + optional_graph_unit2 | graph_unit2)
-            + pynutil.insert("\"")
+            pynutil.insert('units: "') + (graph_unit_plural + optional_graph_unit2 | graph_unit2) + pynutil.insert('"')
         )
 
         unit_singular = (
-            pynutil.insert("units: \"") + (graph_unit + optional_graph_unit2 | graph_unit2) + pynutil.insert("\"")
+            pynutil.insert('units: "') + (graph_unit + optional_graph_unit2 | graph_unit2) + pynutil.insert('"')
         )
 
         subgraph_decimal = (
@@ -102,18 +102,18 @@ class MeasureFst(GraphFst):
             + decimal.final_graph_wo_negative
             + delete_space
             + pynutil.insert(" } ")
-            + pynutil.insert("units: \"")
+            + pynutil.insert('units: "')
             + pynini.union("AM", "FM")
-            + pynutil.insert("\"")
+            + pynutil.insert('"')
         )
 
         subgraph_cardinal = (
             pynutil.insert("cardinal { ")
             + optional_graph_negative
-            + pynutil.insert("integer: \"")
+            + pynutil.insert('integer: "')
             + ((NEMO_SIGMA - "1") @ cardinal_graph)
             + delete_space
-            + pynutil.insert("\"")
+            + pynutil.insert('"')
             + pynutil.insert(" } ")
             + unit_plural
         )
@@ -121,44 +121,44 @@ class MeasureFst(GraphFst):
         subgraph_cardinal |= (
             pynutil.insert("cardinal { ")
             + optional_graph_negative
-            + pynutil.insert("integer: \"")
+            + pynutil.insert('integer: "')
             + pynini.cross("1", "one")
             + delete_space
-            + pynutil.insert("\"")
+            + pynutil.insert('"')
             + pynutil.insert(" } ")
             + unit_singular
         )
 
         unit_graph = (
-            pynutil.insert("cardinal { integer: \"-\" } units: \"")
+            pynutil.insert('cardinal { integer: "-" } units: "')
             + ((pynini.cross("/", "per") + delete_zero_or_one_space) | (pynini.accep("per") + pynutil.delete(" ")))
             + pynutil.insert(NEMO_NON_BREAKING_SPACE)
             + graph_unit
-            + pynutil.insert("\" preserve_order: true")
+            + pynutil.insert('" preserve_order: true')
         )
 
         decimal_dash_alpha = (
             pynutil.insert("decimal { ")
             + decimal.final_graph_wo_negative
-            + pynini.cross('-', '')
-            + pynutil.insert(" } units: \"")
+            + pynini.cross("-", "")
+            + pynutil.insert(' } units: "')
             + pynini.closure(NEMO_ALPHA, 1)
-            + pynutil.insert("\"")
+            + pynutil.insert('"')
         )
 
         decimal_times = (
             pynutil.insert("decimal { ")
             + decimal.final_graph_wo_negative
-            + pynutil.insert(" } units: \"")
-            + (pynini.cross(pynini.union('x', "X"), 'x') | pynini.cross(pynini.union('x', "X"), ' times'))
-            + pynutil.insert("\"")
+            + pynutil.insert(' } units: "')
+            + (pynini.cross(pynini.union("x", "X"), "x") | pynini.cross(pynini.union("x", "X"), " times"))
+            + pynutil.insert('"')
         )
 
         alpha_dash_decimal = (
-            pynutil.insert("units: \"")
+            pynutil.insert('units: "')
             + pynini.closure(NEMO_ALPHA, 1)
-            + pynini.accep('-')
-            + pynutil.insert("\"")
+            + pynini.accep("-")
+            + pynutil.insert('"')
             + pynutil.insert(" decimal { ")
             + decimal.final_graph_wo_negative
             + pynutil.insert(" } preserve_order: true")
@@ -170,9 +170,9 @@ class MeasureFst(GraphFst):
 
         address = self.get_address_graph(cardinal)
         address = (
-            pynutil.insert("units: \"address\" cardinal { integer: \"")
+            pynutil.insert('units: "address" cardinal { integer: "')
             + address
-            + pynutil.insert("\" } preserve_order: true")
+            + pynutil.insert('" } preserve_order: true')
         )
 
         math_operations = pynini.string_file(get_abs_path("data/measure/math_operation.tsv"))
@@ -203,9 +203,7 @@ class MeasureFst(GraphFst):
         )
 
         math = (
-            pynutil.insert("units: \"math\" cardinal { integer: \"")
-            + math
-            + pynutil.insert("\" } preserve_order: true")
+            pynutil.insert('units: "math" cardinal { integer: "') + math + pynutil.insert('" } preserve_order: true')
         )
         final_graph = (
             subgraph_decimal
@@ -252,7 +250,7 @@ class MeasureFst(GraphFst):
         ordinal_verbalizer = OrdinalVerbalizer().graph
         ordinal_tagger = OrdinalTagger(cardinal=cardinal).graph
         ordinal_num = pynini.compose(
-            pynutil.insert("integer: \"") + ordinal_tagger + pynutil.insert("\""), ordinal_verbalizer
+            pynutil.insert('integer: "') + ordinal_tagger + pynutil.insert('"'), ordinal_verbalizer,
         )
 
         address_num = NEMO_DIGIT ** (1, 2) @ cardinal.graph_hundred_component_at_least_one_none_zero_digit
@@ -298,6 +296,24 @@ class MeasureFst(GraphFst):
         zip_code = pynini.closure(pynini.closure(pynini.accep(","), 0, 1) + pynini.accep(NEMO_SPACE) + zip_code, 0, 1,)
 
         address = address_num + direction + address_words + pynini.closure(city + state + zip_code, 0, 1)
+
+        # Handles sentences ending in terminal period abbreviations, when followed by another sentence
+        # (e.g. Nancy lived on 1428 Elm St. It was a strange place-> Nancy lived on fourteen twenty eight Elm Street. It was a strange place.)
+
+        hyphen = pynini.accep("-")
+        alpha_chars = NEMO_ALPHA | hyphen
+
+        address_eos = (
+            address_num
+            + direction
+            + address_words
+            + pynini.accep(".")
+            + pynini.closure(NEMO_SPACE, 1, 2)
+            + NEMO_UPPER
+            + pynini.closure(alpha_chars)
+        )
+
+        address |= pynutil.add_weight(address_eos, -0.001)
 
         address |= address_num + direction + address_words + pynini.closure(pynini.cross(".", ""), 0, 1)
 
