27a460b52c81dcb812ff38ff174c640568811a70
Alex Cui <alexcui1994@gmail.com>
2024-08-26 18:39:52 -0700

    tn support
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/ja/test_cardinal.py
+++ tests/nemo_text_processing/ja/test_cardinal.py
@@ -33,7 +33,7 @@ class TestCardinal:
 
     inverse_normalizer_ja = InverseNormalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False)
 
-    @parameterized.expand(parse_test_case_file('ja/data_inverse_text_normalization/test_cases_date.txt'))
+    @parameterized.expand(parse_test_case_file('ja/data_inverse_text_normalization/test_cases_cardinal.txt'))
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_denorm(self, test_input, expected):
