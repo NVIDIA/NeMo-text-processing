5bc9572c2a0a73d867edf63151d358c43b3a4419
Alex Cui <alexcui1994@gmail.com>
2024-08-27 19:16:14 -0700

    test case updates on jp tn
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/ja/test_sparrowhawk_normalization.sh
+++ tests/nemo_text_processing/ja/test_sparrowhawk_normalization.sh
@@ -22,6 +22,7 @@ runtest () {
     assertEquals "$written" "$spoken" "$denorm_pred"
   done < "$input"
 }
+<<<<<<< HEAD
 testTNFractionText() {
   input=$PROJECT_DIR/ja/data_text_normalization/test_cases_fraction.txt
   runtest $input
@@ -46,6 +47,33 @@ testTNDateText() {
   input=$PROJECT_DIR/ja/data_text_normalization/test_cases_date.txt
   runtest $input
 }
+=======
+
+# testTNTimeText() {
+#   input=$PROJECT_DIR/zh/data_text_normalization/test_cases_time.txt
+#   runtest $input
+# }
+# testTNCardinalText() {
+#   input=$PROJECT_DIR/zh/data_text_normalization/test_cases_cardinal.txt
+#   runtest $input
+# }
+# testTNOrdinalText() {
+#   input=$PROJECT_DIR/zh/data_text_normalization/test_cases_ordinal.txt
+#   runtest $input
+# }
+# testTNDecimalalText() {
+#  input=$PROJECT_DIR/zh/data_text_normalization/test_cases_decimal.txt
+#   runtest $input
+# }
+#testTNFractionText() {
+#  input=$PROJECT_DIR/ja/data_text_normalization/test_cases_fraction.txt
+#  runtest $input
+#}
+testTNDateText() {
+   input=$PROJECT_DIR/ja/data_text_normalization/test_cases_date.txt
+   runtest $input
+}
+>>>>>>> cffec567 (test case updates on jp tn)
 # testTNMoneyText() {
 #   input=$PROJECT_DIR/zh/data_text_normalization/test_cases_money.txt
 #   runtest $input
