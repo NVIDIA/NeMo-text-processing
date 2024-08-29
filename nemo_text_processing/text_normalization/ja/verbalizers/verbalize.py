fba083bb602caabf03f19cc0e06b5801a94bfd83
Alex Cui <alexcui1994@gmail.com>
2024-08-29 11:17:47 -0700

    updates for post_processing and verbalizer
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/ja/verbalizers/verbalize.py
+++ nemo_text_processing/text_normalization/ja/verbalizers/verbalize.py
@@ -57,6 +57,11 @@ class VerbalizeFst(GraphFst):
         word = WordFst(deterministic=deterministic)
         fraction = FractionFst(deterministic=deterministic)
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 6e5106f (updates for post_processing and verbalizer)
         # money = MoneyFst(decimal=decimal, deterministic=deterministic)
         # measure = MeasureFst(cardinal=cardinal, decimal=decimal, fraction=fraction, deterministic=deterministic)
         time = TimeFst(deterministic=deterministic)
