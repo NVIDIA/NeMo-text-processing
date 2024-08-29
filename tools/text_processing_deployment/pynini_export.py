90209dd6ad8c88832fbfdbb5e212a354e2cf356f
Alex Cui <alexcui1994@gmail.com>
2024-08-29 11:18:44 -0700

    add ja
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tools/text_processing_deployment/pynini_export.py
+++ tools/text_processing_deployment/pynini_export.py
@@ -86,15 +86,7 @@ def parse_args():
     parser.add_argument(
         "--language",
         help="language",
-<<<<<<< HEAD
-<<<<<<< HEAD
         choices=["en", "de", "es", "pt", "ru", 'fr', 'hu', 'sv', 'vi', 'zh', 'ar', 'it', 'es_en', 'hy', 'mr', 'ja'],
-=======
-        choices=["en", "de", "es", "pt", "ru", 'fr', 'hu', 'sv', 'vi', 'zh', 'ar', 'it', 'es_en', 'ja'],
->>>>>>> 68b711b (updates for japanese tn)
-=======
-        choices=["en", "de", "es", "pt", "ru", 'fr', 'hu', 'sv', 'vi', 'zh', 'ar', 'it', 'es_en', 'ja'],
->>>>>>> 68b711b (updates for japanese tn)
         type=str,
         default='en',
     )
