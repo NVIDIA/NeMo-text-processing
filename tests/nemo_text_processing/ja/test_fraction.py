52d2c723a0a6504f743b1e4279e1f641170e9d4e
Alex Cui <alexcui1994@gmail.com>
2024-08-30 08:25:30 -0700

    add test for date tn, fraction tn, and time tn
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/ja/test_fraction.py
+++ tests/nemo_text_processing/ja/test_fraction.py
@@ -16,6 +16,7 @@ import pytest
 from parameterized import parameterized
 
 from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
+from nemo_text_processing.text_normalization.normalize import Normalizer
 
 from ..utils import CACHE_DIR, parse_test_case_file
 
@@ -29,4 +30,13 @@ class TestFraction:
     @pytest.mark.unit
     def test_denorm(self, test_input, expected):
         pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
+        assert pred == expected
+
+    normalizer = Normalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
+
+    @parameterized.expand(parse_test_case_file('ja/data_text_normalization/test_cases_fraction.txt'))
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_denorm(self, test_input, expected):
+        pred = self.normalizer.normalize(test_input, verbose=False)
         assert pred == expected
