a48bba101f6720c142950065841bb79443f86170
Alex Cui <alexcui1994@gmail.com>
2024-08-30 08:25:30 -0700

    add test for date tn, fraction tn, and time tn
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/ja/test_date.py
+++ tests/nemo_text_processing/ja/test_date.py
@@ -23,24 +23,12 @@ from ..utils import CACHE_DIR, parse_test_case_file
 
 class TestDate:
 
-    # inverse_normalizer = InverseNormalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False)
+    inverse_normalizer = InverseNormalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False)
 
-    # @parameterized.expand(parse_test_case_file('ja/data_inverse_text_normalization/test_cases_date.txt'))
-    # @pytest.mark.run_only_on('CPU')
-    # @pytest.mark.unit
-    # def test_denorm(self, test_input, expected):
-    #     pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
-    #     assert pred == expected
-
-
-    normalizer = Normalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
-
-    @parameterized.expand(parse_test_case_file('ja/data_text_normalization/test_cases_date.txt'))
+    @parameterized.expand(parse_test_case_file('ja/data_inverse_text_normalization/test_cases_date.txt'))
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_denorm(self, test_input, expected):
-<<<<<<< HEAD
-=======
         pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
         assert pred == expected
 
@@ -51,6 +39,5 @@ class TestDate:
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_denorm(self, test_input, expected):
->>>>>>> eaa5ab17 (add test for date tn, fraction tn, and time tn)
         pred = self.normalizer.normalize(test_input, verbose=False)
         assert pred == expected
