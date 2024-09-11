8232404bb8386046796ad1fa257270fa8ad5abb1
Ankit Narwade <anarwade@nvidia.com>
2024-09-11 11:52:13 +0530

    Cleanup
    Signed-off-by: Ankit Narwade <anarwade@nvidia.com>

--- tests/nemo_text_processing/hi/test_time.py
+++ tests/nemo_text_processing/hi/test_time.py
@@ -21,10 +21,10 @@ from nemo_text_processing.text_normalization.normalize_with_audio import Normali
 from ..utils import CACHE_DIR, RUN_AUDIO_BASED_TESTS, parse_test_case_file
 
 
-class TestMeasure:
+class TestTime:
     normalizer = Normalizer(input_case='cased', lang='hi', cache_dir=CACHE_DIR, overwrite_cache=False)
 
-    @parameterized.expand(parse_test_case_file('hi/data_text_normalization/test_cases_measure.txt'))
+    @parameterized.expand(parse_test_case_file('hi/data_text_normalization/test_cases_time.txt'))
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_norm(self, test_input, expected):
