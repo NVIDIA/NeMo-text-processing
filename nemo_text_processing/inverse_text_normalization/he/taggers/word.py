c9aa957a2825570b03d622a7fd70e41772be8042
dankeinan1 <dan.keinan@verbit.ai>
2024-09-13 13:45:29 +0200

    Hebrew ITN - formatting files
    Signed-off-by: dankeinan1 <dan.keinan@verbit.ai>

--- nemo_text_processing/inverse_text_normalization/he/taggers/word.py
+++ nemo_text_processing/inverse_text_normalization/he/taggers/word.py
@@ -14,6 +14,7 @@
 
 import pynini
 from pynini.lib import pynutil
+
 from nemo_text_processing.inverse_text_normalization.he.graph_utils import NEMO_NOT_SPACE, GraphFst
 
 
--- nemo_text_processing/inverse_text_normalization/he/verbalizers/ordinal.py
+++ nemo_text_processing/inverse_text_normalization/he/verbalizers/ordinal.py
@@ -14,6 +14,7 @@
 
 import pynini
 from pynini.lib import pynutil
+
 from nemo_text_processing.inverse_text_normalization.he.graph_utils import NEMO_DIGIT, GraphFst, delete_space
 
 
