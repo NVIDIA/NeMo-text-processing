70d9234d2f867cb6e81138f273c604d33bbeda6a
Alex Cui <alexcui1994@gmail.com>
2024-09-19 09:36:24 -0700

    uncommenting itn tests
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- tests/nemo_text_processing/ja/test_time.py
+++ tests/nemo_text_processing/ja/test_time.py
@@ -23,22 +23,13 @@ from ..utils import CACHE_DIR, parse_test_case_file
 
 class TestCardinal:
 
-    # inverse_normalizer = InverseNormalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False)
+    inverse_normalizer = InverseNormalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False)
 
-    # @parameterized.expand(parse_test_case_file('ja/data_inverse_text_normalization/test_cases_time.txt'))
-    # @pytest.mark.run_only_on('CPU')
-    # @pytest.mark.unit
-    # def test_denorm(self, test_input, expected):
-    #     pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
-    #     assert pred == expected
-
-    normalizer = Normalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
-
-    @parameterized.expand(parse_test_case_file('ja/data_text_normalization/test_cases_time.txt'))
+    @parameterized.expand(parse_test_case_file('ja/data_inverse_text_normalization/test_cases_time.txt'))
     @pytest.mark.run_only_on('CPU')
     @pytest.mark.unit
     def test_denorm(self, test_input, expected):
-        pred = self.normalizer.normalize(test_input, verbose=False)
+        pred = self.inverse_normalizer.inverse_normalize(test_input, verbose=False)
         assert pred == expected
 
     normalizer = Normalizer(lang='ja', cache_dir=CACHE_DIR, overwrite_cache=False, input_case='cased')
@@ -49,3 +40,4 @@ class TestCardinal:
     def test_denorm(self, test_input, expected):
         pred = self.normalizer.normalize(test_input, verbose=False)
         assert pred == expected
+
