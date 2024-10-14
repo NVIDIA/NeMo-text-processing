5748bd75db7919adfbbb560bd391008e2dd6aed6
Simon Zuberek <szuberek@nvidia.com>
2024-10-14 14:39:23 -0500

    Implements the fix
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>

--- nemo_text_processing/text_normalization/de/taggers/electronic.py
+++ nemo_text_processing/text_normalization/de/taggers/electronic.py
@@ -17,7 +17,12 @@ import pynini
 from pynini.lib import pynutil
 
 from nemo_text_processing.text_normalization.de.utils import get_abs_path, load_labels
-from nemo_text_processing.text_normalization.en.graph_utils import NEMO_ALPHA, NEMO_DIGIT, GraphFst, insert_space
+from nemo_text_processing.text_normalization.en.graph_utils import (
+    NEMO_ALPHA,
+    NEMO_DIGIT,
+    GraphFst,
+    insert_space,
+)
 
 
 class ElectronicFst(GraphFst):
@@ -32,14 +37,20 @@ class ElectronicFst(GraphFst):
     """
 
     def __init__(self, deterministic: bool = True):
-        super().__init__(name="electronic", kind="classify", deterministic=deterministic)
+        super().__init__(
+            name="electronic", kind="classify", deterministic=deterministic
+        )
 
         dot = pynini.accep(".")
 
-        symbols = [x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))]
+        symbols = [
+            x[0] for x in load_labels(get_abs_path("data/electronic/symbols.tsv"))
+        ]
         symbols = pynini.union(*symbols)
         # all symbols
-        symbols_no_period = pynini.difference(symbols, dot)  # alphabet of accepted symbols excluding the '.'
+        symbols_no_period = pynini.difference(
+            symbols, dot
+        )  # alphabet of accepted symbols excluding the '.'
         accepted_characters = pynini.closure(
             (NEMO_ALPHA | NEMO_DIGIT | symbols_no_period), 1
         )  # alphabet of accepted chars excluding the '.'
@@ -50,11 +61,19 @@ class ElectronicFst(GraphFst):
         # domains
         domain = dot + accepted_characters
         domain_graph = (
-            pynutil.insert('domain: "') + (accepted_characters + pynini.closure(domain, 1)) + pynutil.insert('"')
+            pynutil.insert('domain: "')
+            + (accepted_characters + pynini.closure(domain, 1))
+            + dot.ques
+            + pynutil.insert('"')
         )
 
         # email
-        username = pynutil.insert('username: "') + all_characters + pynutil.insert('"') + pynini.cross("@", " ")
+        username = (
+            pynutil.insert('username: "')
+            + all_characters
+            + pynutil.insert('"')
+            + pynini.cross("@", " ")
+        )
         email = username + domain_graph
 
         # social media tags
@@ -62,6 +81,7 @@ class ElectronicFst(GraphFst):
             pynini.cross("@", "")
             + pynutil.insert('username: "')
             + (accepted_characters | (accepted_characters + pynini.closure(domain, 1)))
+            + dot.ques
             + pynutil.insert('"')
         )
 
@@ -75,5 +95,7 @@ class ElectronicFst(GraphFst):
         graph = url | domain_graph | email | tag
         self.graph = graph
 
-        final_graph = self.add_tokens(self.graph + pynutil.insert(" preserve_order: true"))
+        final_graph = self.add_tokens(
+            self.graph + pynutil.insert(" preserve_order: true")
+        )
         self.fst = final_graph.optimize()
