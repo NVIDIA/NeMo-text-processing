52077e8d8d240205f29301822eeff056247ee9f0
Alex Cui <alexcui1994@gmail.com>
2024-10-16 12:00:25 -0700

    ja tn
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

---
+++ nemo_text_processing/text_normalization/ja/verbalizers/verbalize.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import pynini
+from pynini.lib import pynutil
+
+from nemo_text_processing.text_normalization.ja.graph_utils import (
+    NEMO_NARROW_NON_BREAK_SPACE,
+    NEMO_NON_BREAKING_SPACE,
+    GraphFst,
+    delete_space,
+)
+from nemo_text_processing.text_normalization.ja.verbalizers.cardinal import CardinalFst
+from nemo_text_processing.text_normalization.ja.verbalizers.date import DateFst
+from nemo_text_processing.text_normalization.ja.verbalizers.decimal import DecimalFst
+from nemo_text_processing.text_normalization.ja.verbalizers.fraction import FractionFst
+
+# from nemo_text_processing.text_normalization.ja.verbalizers.measure import MeasureFst
+# from nemo_text_processing.text_normalization.ja.verbalizers.money import MoneyFst
+from nemo_text_processing.text_normalization.ja.verbalizers.ordinal import OrdinalFst
+from nemo_text_processing.text_normalization.ja.verbalizers.time import TimeFst
+from nemo_text_processing.text_normalization.ja.verbalizers.whitelist import WhiteListFst
+from nemo_text_processing.text_normalization.ja.verbalizers.word import WordFst
+
+
+class VerbalizeFst(GraphFst):
+    """
+    Composes other verbalizer grammars.
+    For deployment, this grammar will be compiled and exported to OpenFst Finate State Archiv (FAR) File. 
+    More details to deployment at NeMo/tools/text_processing_deployment.
+    Args:
+        deterministic: if True will provide a single transduction option,
+            for False multiple options (used for audio-based normalization)
+    """
+
+    def __init__(self, deterministic: bool = True):
+        super().__init__(name="verbalize", kind="verbalize", deterministic=deterministic)
+
+        space = pynini.closure(NEMO_NARROW_NON_BREAK_SPACE) | pynini.closure(NEMO_NON_BREAKING_SPACE)
+
+        date = DateFst(deterministic=deterministic)
+        cardinal = CardinalFst(deterministic=deterministic)
+        ordinal = OrdinalFst(deterministic=deterministic)
+        decimal = DecimalFst(deterministic=deterministic)
+        word = WordFst(deterministic=deterministic)
+        fraction = FractionFst(deterministic=deterministic)
+
+        # money = MoneyFst(decimal=decimal, deterministic=deterministic)
+        # measure = MeasureFst(cardinal=cardinal, decimal=decimal, fraction=fraction, deterministic=deterministic)
+        time = TimeFst(deterministic=deterministic)
+        whitelist = WhiteListFst(deterministic=deterministic)
+
+        graph = pynini.union(
+            date.fst,
+            cardinal.fst,
+            ordinal.fst,
+            decimal.fst,
+            fraction.fst,
+            word.fst,
+            #   money.fst,
+            #   measure.fst,
+            time.fst,
+            whitelist.fst,
+        )
+        graph = pynini.closure(delete_space) + graph + pynini.closure(delete_space)
+
+<<<<<<< HEAD
+        self.fst = graph.optimize()
+=======
+        self.fst = graph.optimize()
+>>>>>>> 9ba0b2d9aa8af55b2ae639c5d17742be4b5a5fcc
