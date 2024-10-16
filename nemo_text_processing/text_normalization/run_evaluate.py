0a72d98e9af43a417db3d983c86936480f05d0a9
Alex Cui <alexcui1994@gmail.com>
2024-10-16 12:01:09 -0700

    adding ja
    Signed-off-by: Alex Cui <alexcui1994@gmail.com>

--- nemo_text_processing/text_normalization/run_evaluate.py
+++ nemo_text_processing/text_normalization/run_evaluate.py
@@ -35,7 +35,7 @@ def parse_args():
     parser.add_argument(
         "--lang",
         help="language",
-        choices=['ar', 'de', 'en', 'es', 'fr', 'hu', 'it', 'ru', 'sv', 'zh', 'hy'],
+        choices=['ar', 'de', 'en', 'es', 'fr', 'hu', 'it', 'ru', 'sv', 'zh', 'hy', "ja"],
         default="en",
         type=str,
     )
