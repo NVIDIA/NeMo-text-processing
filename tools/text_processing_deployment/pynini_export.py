ec1fd4ecfc48fcff7ad0128f0fa5ce6ef2454b97
Ankit Narwade <anarwade@nvidia.com>
2024-10-28 19:20:54 +0530

    28/10/2024 - Cardinals up to a hundred trillions, timeFST and transliteration (#209)
    * Cardinals up to a hundred trillions, timeFST and transliteration
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    * Cardinals up to a hundred trillions, timeFST and transliteration
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    * Cardinals up to a hundred trillions, timeFST and transliteration (moving constants to data files).
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    * Update test_cases_word.txt
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    * Update graph_utils.py
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    * Cardinals up to a hundred trillions, timeFST and transliteration - reformatteda
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    * Disabled Black during formatting.
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    
    ---------
    
    Signed-off-by: kurt0cougar <kkleber000@gmail.com>
    Signed-off-by: Ankit Narwade <anarwade@nvidia.com>

--- tools/text_processing_deployment/pynini_export.py
+++ tools/text_processing_deployment/pynini_export.py
@@ -302,6 +302,10 @@ if __name__ == '__main__':
         from nemo_text_processing.text_normalization.rw.taggers.tokenize_and_classify import (
             ClassifyFst as TNClassifyFst,
         )
+    elif args.language == 'rw':
+        from nemo_text_processing.text_normalization.rw.taggers.tokenize_and_classify import (
+            ClassifyFst as TNClassifyFst,
+        )
         from nemo_text_processing.text_normalization.rw.verbalizers.verbalize import VerbalizeFst as TNVerbalizeFst
     output_dir = os.path.join(args.output_dir, f"{args.language}_{args.grammars}_{args.input_case}")
     export_grammars(
