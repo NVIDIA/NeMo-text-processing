bed4afcc0946841b0a2cbcb2269cfd8eedbdf030
Namrata Gachchi <ngachchi@nvidia.com>
2024-11-18 20:25:56 +0530

    Hindi TN Support for Cardinal, Decimal, Fraction, Date, Time, Money and Measure (#241)
    * Hindi TN changes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updated date for Hindi TN cache
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * additional whitelist class .tsv files and unused imports removed
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * incorporated suggestions for unused statements and another for closing the file opened
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Combined Hindi TN and ITN seperate blocks into single
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added init.py files and removed unused commented lines
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * commented irrevelant references and unused snippets from whitelist and word file
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Whitelist and Word class changes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * post processor changes with minor fixes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * remove space before punctuation for sparrowhawk file
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * minor fixes for measure class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updated Jenkinsfile
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * removed unused imports and statements
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updated date stamp for HI cache and commented ITN grammars
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Updates the cache
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Disables Hindi ITN L0 checks
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Reapplies ITN CI Checks
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Adds missing inits
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * resolved the failing sparrowhawk test cases failed
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    ---------
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Simon Zuberek <szuberek@nvidia.com>

---
+++ nemo_text_processing/text_normalization/hi/taggers/time.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pynini
+from pynini.lib import pynutil
+
+from nemo_text_processing.text_normalization.hi.graph_utils import GraphFst, insert_space
+from nemo_text_processing.text_normalization.hi.utils import get_abs_path
+
+hours_graph = pynini.string_file(get_abs_path("data/time/hours.tsv"))
+minutes_graph = pynini.string_file(get_abs_path("data/time/minutes.tsv"))
+seconds_graph = pynini.string_file(get_abs_path("data/time/seconds.tsv"))
+
+
+class TimeFst(GraphFst):
+    """
+    Finite state transducer for classifying time, e.g.
+        १२:३०:३०  -> time { hours: "बारह" minutes: "तीस" seconds: "तीस" }
+        १:४०  -> time { hours: "एक" minutes: "चालीस" }
+        १:००  -> time { hours: "एक" }
+         
+    Args:
+        time: GraphFst
+        deterministic: if True will provide a single transduction option,
+            for False multiple transduction are generated (used for audio-based normalization)
+    """
+
+    def __init__(self):
+        super().__init__(name="time", kind="classify")
+
+        delete_colon = pynutil.delete(":")
+
+        self.hours = pynutil.insert("hours: \"") + hours_graph + pynutil.insert("\" ")
+        self.minutes = pynutil.insert("minutes: \"") + minutes_graph + pynutil.insert("\" ")
+        self.seconds = pynutil.insert("seconds: \"") + seconds_graph + pynutil.insert("\" ")
+
+        # hour minute seconds
+        graph_hms = (
+            self.hours + delete_colon + insert_space + self.minutes + delete_colon + insert_space + self.seconds
+        )
+
+        # hour minute
+        graph_hm = self.hours + delete_colon + insert_space + self.minutes
+
+        # hour
+        graph_h = self.hours + delete_colon + pynutil.delete("००")
+
+        final_graph = graph_hms | graph_hm | graph_h
+
+        final_graph = self.add_tokens(final_graph)
+        self.fst = final_graph.optimize()
