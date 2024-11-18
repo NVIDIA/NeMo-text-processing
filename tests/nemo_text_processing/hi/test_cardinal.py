8be056d8a32df1c0f446448fa70e1bd3be0cea09
Namrata Gachchi <ngachchi@nvidia.com>
2024-11-18 20:25:56 +0530

    Hindi TN Support for Cardinal, Decimal, Fraction, Date, Time, Money and Measure (#241)
    * Hindi TN changes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updated date for Hindi TN cache
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * additional whitelist class .tsv files and unused imports removed
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * incorporated suggestions for unused statements and another for closing the file opened
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Combined Hindi TN and ITN seperate blocks into single
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added init.py files and removed unused commented lines
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * commented irrevelant references and unused snippets from whitelist and word file
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Whitelist and Word class changes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * post processor changes with minor fixes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * remove space before punctuation for sparrowhawk file
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * minor fixes for measure class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updated Jenkinsfile
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * removed unused imports and statements
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updated date stamp for HI cache and commented ITN grammars
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Updates the cache
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Disables Hindi ITN L0 checks
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Reapplies ITN CI Checks
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * Adds missing inits
    
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    
    * resolved the failing sparrowhawk test cases failed
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    ---------
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    Signed-off-by: Simon Zuberek <szuberek@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Simon Zuberek <szuberek@nvidia.com>
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>

--- tests/nemo_text_processing/hi/test_cardinal.py
+++ tests/nemo_text_processing/hi/test_cardinal.py
@@ -33,3 +33,10 @@ class TestCardinal:
     def test_norm(self, test_input, expected):
         pred = self.normalizer.normalize(test_input, verbose=False)
         assert pred.strip() == expected.strip()
+
+    @parameterized.expand(parse_test_case_file('hi/data_inverse_text_normalization/test_cases_cardinal.txt'))
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_norm(self, test_input, expected):
+        pred = self.normalizer.normalize(test_input, verbose=False)
+        assert pred.strip() == expected.strip()
