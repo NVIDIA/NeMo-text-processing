6c3eedfdf39744a8c97b19a74d1faf3bd475f0b8
Namrata Gachchi <ngachchi@nvidia.com>
2024-12-06 10:38:03 +0530

    fixed the sparrowhawk to trim extra space
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>

--- tests/nemo_text_processing/hi/test_sparrowhawk_normalization.sh
+++ tests/nemo_text_processing/hi/test_sparrowhawk_normalization.sh
@@ -14,8 +14,8 @@ runtest () {
     denorm_pred=$(echo $written | normalizer_main --config=sparrowhawk_configuration.ascii_proto 2>&1 | tail -n 1 | sed 's/\xC2\xA0/ /g')
 
     # trim white space
-    spoken="$(echo -e "${spoken}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-    denorm_pred="$(echo -e "${denorm_pred}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    spoken="$(echo "${spoken}" | tr -d ' ')"
+    denorm_pred="$(echo "${denorm_pred}" | tr -d ' ')"
 
     # input expected actual
     assertEquals "$written" "$spoken" "$denorm_pred"
