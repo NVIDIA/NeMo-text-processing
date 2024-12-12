ba19d36ced05b630b88a4a9c404b01a0a5208442
Tarushi V <tarushiv@nvidia.com>
2024-12-12 15:54:29 +0530

    Future implementations for date
    Signed-off-by: Tarushi V <tarushiv@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/hi/taggers/date.py
+++ nemo_text_processing/inverse_text_normalization/hi/taggers/date.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 import pynini
 from pynini.lib import pynutil
-
+ 
 from nemo_text_processing.inverse_text_normalization.hi.graph_utils import (
     NEMO_HI_DIGIT,
     GraphFst,
@@ -22,9 +22,9 @@ from nemo_text_processing.inverse_text_normalization.hi.graph_utils import (
     delete_space,
     insert_space,
 )
-from nemo_text_processing.inverse_text_normalization.hi.utils import get_abs_path
-
-
+from nemo_text_processing.inverse_text_normalization.hi.utils import get_abs_path, apply_fst
+ 
+ 
 class DateFst(GraphFst):
     """
         Finite state transducer for classifying date, 
@@ -34,22 +34,25 @@ class DateFst(GraphFst):
         cardinal: CardinalFst
         date: DateFst
     """
-
+ 
     def __init__(self, cardinal: GraphFst):
         super().__init__(name="date", kind="classify")
-
+ 
         graph_year = pynutil.add_weight(
             pynini.compose(cardinal.graph_no_exception, pynini.closure(NEMO_HI_DIGIT, 1, 4)), 0.03
         )
-
+ 
         month_graph = pynini.string_file(get_abs_path("data/date/months.tsv"))
         graph_date_days = pynini.string_file(get_abs_path("data/date/date_days.tsv")).invert()
-
+        graph_century = pynini.string_file(get_abs_path("data/date/century.tsv")).invert()
+ 
+ 
         self.day = pynutil.insert("day: \"") + graph_date_days + pynutil.insert("\" ")
         self.month = pynutil.insert("month: \"") + month_graph + pynutil.insert("\" ")
         self.year = pynutil.insert("year: \"") + graph_year + pynutil.insert("\" ")
+        self.century = pynutil.insert("text: \"") + graph_century + pynutil.insert("\" ")
         insert_comma = pynutil.insert(", ")
-
+ 
         graph_day_month = self.day + delete_space + self.month
         graph_month_day = self.month + delete_space + self.day
         graph_month_day += pynutil.insert(" preserve_order: true")
@@ -58,9 +61,28 @@ class DateFst(GraphFst):
         graph_month_day_year += pynutil.insert(" preserve_order: true")
         graph_month_year = self.month + delete_space + self.year
         graph_saal = self.year
-
-        graph = graph_day_month | graph_month_day | graph_day_month_year | graph_month_day_year | graph_month_year
-        self.graph = graph.optimize()
-
+        graph_AD_BC = self.year + delete_space + self.century
+        graph_day_month_year_century = self.day + delete_space + self.month + delete_space + self.year + delete_space + self.century
+        graph_month_year_century = self.month + delete_space + self.year + delete_space + self.century
+        graph_year_range = self.year + delete_space + pynutil.delete("से") + delete_space + self.year
+ 
+ 
+        graph = graph_day_month | graph_month_day | graph_day_month_year | graph_month_day_year | graph_month_year | graph_saal | graph_AD_BC | graph_day_month_year_century | graph_month_year_century | graph_year_range
         final_graph = self.add_tokens(graph)
         self.fst = final_graph
+
+#from nemo_text_processing.inverse_text_normalization.hi.taggers.cardinal import CardinalFst
+#cardinal = CardinalFst()
+#date = DateFst(cardinal)
+#input_text = "पच्चीस मार्च दो हज़ार दस"
+#input_text = "छ: मार्च उन्नीस सौ नब्बे"
+#input_text = "छ: मार्च उन्नीस सौ नब्बे ईस्वी"
+#input_text = "छह मार्च दो हज़ार दस"
+#input_text = "तीन फ़रवरी"
+#input_text = "चौवालीस सौ ईसा पूर्व"
+#input_text = "फ़रवरी चौवालीस सौ ईसा पूर्व"
+#input_text = "चौवालीस सौ ईस्वी"
+#input_text = "उन्नीस सौ बीस से उन्नीस सौ छब्बीस"
+#input_text = "उन्नीस सौ बीस से छब्बीस"
+#output = apply_fst(input_text, date.fst)
+#print(output)
--- nemo_text_processing/inverse_text_normalization/hi/verbalizers/date.py
+++ nemo_text_processing/inverse_text_normalization/hi/verbalizers/date.py
@@ -11,25 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+ 
 import pynini
 from pynini.lib import pynutil
-
+ 
 from nemo_text_processing.inverse_text_normalization.hi.graph_utils import (
     NEMO_NOT_QUOTE,
     GraphFst,
     delete_extra_space,
     delete_space,
 )
-
-
+ 
+from nemo_text_processing.inverse_text_normalization.hi.utils import apply_fst
+ 
+ 
 class DateFst(GraphFst):
     """
     Finite state transducer for verbalizing date, e.g.
         date { month: "जनवरी" day: "५" year: "२०१२" preserve_order: true } -> जनवरी ५ २०१२
         date { day: "५" month: "जनवरी" year: "२०१२" preserve_order: true } -> ५ जनवरी २०१२
     """
-
+ 
     def __init__(self):
         super().__init__(name="date", kind="verbalize")
         month = (
@@ -61,22 +63,32 @@ class DateFst(GraphFst):
             + pynini.closure(NEMO_NOT_QUOTE, 1)
             + pynutil.delete("\"")
         )
-        graph_fy = period + delete_space + year
+        graph_fy = year
+        graph_fy |= period + delete_space + year
+        #century
+        graph_century = year + delete_extra_space + period
         # month (day) year
         graph_mdy = month + delete_extra_space + day + pynutil.insert(",") + delete_extra_space + year
-
+ 
         # (day) month year
         graph_dmy = day + delete_extra_space + month + pynutil.insert(",") + delete_extra_space + year
-
+        # day month year century
+        graph_dmyc = day + delete_extra_space + month + pynutil.insert(",") + delete_extra_space + year + delete_extra_space + period
+ 
         # month year
         graph_my = month + pynini.closure(delete_extra_space + year, 0, 1)
-
+        # month year century
+        graph_myc = month + pynutil.insert(",") + delete_extra_space + year + delete_extra_space + period
+ 
         # month day
         graph_md = month + pynini.closure(delete_extra_space + day, 0, 1)
-
+ 
         # day month
         graph_dm = day + pynini.closure(delete_extra_space + month, 0, 1)
-
+        # date range
+        graph_year_range = year + delete_extra_space + pynutil.insert("-") + delete_extra_space + year
+ 
+ 
         optional_preserve_order = pynini.closure(
             pynutil.delete("preserve_order:") + delete_space + pynutil.delete("true") + delete_space
             | pynutil.delete("field_order:")
@@ -86,12 +98,24 @@ class DateFst(GraphFst):
             + pynutil.delete("\"")
             + delete_space
         )
-
+ 
         final_graph = (
-            (graph_fy | graph_mdy | graph_dmy | graph_my | graph_md | graph_dm)
+            (graph_fy | graph_mdy | graph_dmy | graph_my | graph_md | graph_dm | graph_century | graph_dmyc | graph_myc | graph_year_range)
             + delete_space
             + optional_preserve_order
         )
-
+ 
         delete_tokens = self.delete_tokens(final_graph)
         self.fst = delete_tokens.optimize()
+date = DateFst()
+#input_text = 'date { period: "सन " year: "२०१९"  }'
+#input_text = 'date { day: "१७"month: "अप्रैल"year: "२००२" }'
+#input_text = 'date { day: "२५" month: "मार्च" year: "२०१०"  }'
+#input_text = 'date { day: "१७" month: "अक्टूबर" year: "२०१९"  }'
+#input_text = 'date { year: "४४००"  }'
+#input_text = 'date { year: "४४००" text: "ईस्वी" }'
+#input_text = 'date { year: "४४००" text: "ई. पू."  }'
+#input_text = 'date { day: "२५" month: "मार्च" year: "२०१०" text: "ई. पू." }'
+input_text = 'date { year: "१९२०" year: "१९२६" }'
+output = apply_fst(input_text, date.fst)
+print(output)
