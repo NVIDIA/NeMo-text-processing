41e0a3169086b24245aa16fed1a59b2fb3e00fb9
Namrata Gachchi <ngachchi@nvidia.com>
2025-01-29 16:19:00 +0530

    minor fixes for the tagger files
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>

--- nemo_text_processing/text_normalization/hi/taggers/measure.py
+++ nemo_text_processing/text_normalization/hi/taggers/measure.py
@@ -19,6 +19,11 @@ from nemo_text_processing.text_normalization.hi.graph_utils import GraphFst, del
 from nemo_text_processing.text_normalization.hi.utils import get_abs_path
 
 
+digit = pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
+teens_ties = pynini.string_file(get_abs_path("data/numbers/teens_and_ties.tsv"))
+teens_and_ties = pynutil.add_weight(teens_ties, -0.1)
+
+
 class MeasureFst(GraphFst):
     """
     Finite state transducer for classifying measure, suppletive aware, e.g. 
@@ -35,7 +40,7 @@ class MeasureFst(GraphFst):
     def __init__(self, cardinal: GraphFst, decimal: GraphFst):
         super().__init__(name="measure", kind="classify")
 
-        cardinal_graph = cardinal.final_graph
+        cardinal_graph = digit | teens_and_ties | cardinal.graph_hundreds | cardinal.graph_thousands
         decimal_graph = decimal.final_graph_wo_negative
         unit_graph = pynini.string_file(get_abs_path("data/measure/unit.tsv"))
 
