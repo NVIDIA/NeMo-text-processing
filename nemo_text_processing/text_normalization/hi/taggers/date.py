2d80ba5cf8bcb087e0069a0bb5562ec636a476df
Namrata Gachchi <ngachchi@nvidia.com>
2025-04-22 22:02:28 +0530

    Future Implementations for classes - Measure, Money, and Date (#258)
    * Future Implementations for classes - Measure, Money, and Date
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Resolved the conflicts with mm_yyyy and date ranges and added the previously removed failing test cases.
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * removed the unused empty string implementation
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fixes for the tagger files
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * reformatted decimal final graph
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * incorporated the suggestion for decimal graph
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Century implementations
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Working on the yyyy format for the date class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * reverted yyyy code
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * working on future implementations
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * working on improving the date class accuracy
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added year prefix for the date class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * working on the commma cases for date class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * minor fixes
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * implemented mixed fractions
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * rectified the test case
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * working on quarterly measurements
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * reformatted the prefixes and suffixes for date tagger class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * replaced text tag with era tag for the date class
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    * Removed the text tag reference from date class verbalizer
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    
    ---------
    
    Signed-off-by: Namrata Gachchi <ngachchi@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo_text_processing/text_normalization/hi/taggers/date.py
+++ nemo_text_processing/text_normalization/hi/taggers/date.py
@@ -26,6 +26,20 @@ from nemo_text_processing.text_normalization.hi.utils import get_abs_path
 
 days = pynini.string_file(get_abs_path("data/date/days.tsv"))
 months = pynini.string_file(get_abs_path("data/date/months.tsv"))
+year_suffix = pynini.string_file(get_abs_path("data/date/year_suffix.tsv"))
+digit = pynini.string_file(get_abs_path("data/numbers/digit.tsv"))
+teens_ties = pynini.string_file(get_abs_path("data/numbers/teens_and_ties.tsv"))
+teens_and_ties = pynutil.add_weight(teens_ties, -0.1)
+
+# Read suffixes from file into a list
+with open(get_abs_path("data/date/suffixes.tsv"), "r", encoding="utf-8") as f:
+    suffixes_list = f.read().splitlines()
+with open(get_abs_path("data/date/prefixes.tsv"), "r", encoding="utf-8") as f:
+    prefixes_list = f.read().splitlines()
+
+# Create union of suffixes and prefixes
+suffix_union = pynini.union(*suffixes_list)
+prefix_union = pynini.union(*prefixes_list)
 
 
 class DateFst(GraphFst):
@@ -51,10 +65,15 @@ class DateFst(GraphFst):
             (NEMO_HI_DIGIT + NEMO_HI_NON_ZERO + NEMO_HI_DIGIT + NEMO_HI_DIGIT), cardinal.graph_hundreds_as_thousand
         )
 
+        cardinal_graph = (
+            digit | teens_and_ties | cardinal.graph_hundreds | graph_year_thousands | graph_year_hundreds_as_thousands
+        )
+
         graph_year = graph_year_thousands | graph_year_hundreds_as_thousands
 
         delete_dash = pynutil.delete("-")
         delete_slash = pynutil.delete("/")
+        delete_comma = pynutil.delete(",")
 
         days_graph = pynutil.insert("day: \"") + days + pynutil.insert("\"") + insert_space
 
@@ -68,6 +87,22 @@ class DateFst(GraphFst):
 
         graph_mm_dd += pynutil.insert(" preserve_order: true ")
 
+        # Graph for era
+        era_graph = pynutil.insert("era: \"") + year_suffix + pynutil.insert("\"") + insert_space
+
+        range_graph = pynini.cross("-", "से")
+
+        # Graph for year
+        century_number = pynini.compose(pynini.closure(NEMO_HI_DIGIT, 1), cardinal_graph) + pynini.accep("वीं")
+        century_text = pynutil.insert("era: \"") + century_number + pynutil.insert("\"") + insert_space
+
+        # Updated logic to use suffix_union
+        year_number = graph_year + suffix_union
+        year_text = pynutil.insert("era: \"") + year_number + pynutil.insert("\"") + insert_space
+
+        # Updated logic to use prefix_union
+        year_prefix = pynutil.insert("era: \"") + prefix_union + insert_space + graph_year + pynutil.insert("\"")
+
         graph_dd_mm_yyyy = (
             days_graph + (delete_dash | delete_slash) + months_graph + (delete_dash | delete_slash) + years_graph
         )
@@ -78,7 +113,20 @@ class DateFst(GraphFst):
 
         graph_mm_dd_yyyy += pynutil.insert(" preserve_order: true ")
 
-        graph_mm_yyyy = months_graph + delete_dash + years_graph
+        graph_mm_yyyy = months_graph + delete_dash + insert_space + years_graph
+
+        graph_year_suffix = era_graph
+
+        graph_range = (
+            pynutil.insert("era: \"")
+            + cardinal_graph
+            + insert_space
+            + range_graph
+            + insert_space
+            + cardinal_graph
+            + pynutil.insert("\"")
+            + pynutil.insert(" preserve_order: true ")
+        )
 
         # default assume dd_mm_yyyy
 
@@ -87,7 +135,12 @@ class DateFst(GraphFst):
             | graph_mm_dd
             | pynutil.add_weight(graph_dd_mm_yyyy, -0.001)
             | graph_mm_dd_yyyy
-            | graph_mm_yyyy
+            | pynutil.add_weight(graph_mm_yyyy, -0.2)
+            | pynutil.add_weight(graph_year_suffix, -0.001)
+            | pynutil.add_weight(graph_range, -0.005)
+            | pynutil.add_weight(century_text, -0.001)
+            | pynutil.add_weight(year_text, -0.001)
+            | pynutil.add_weight(year_prefix, -0.009)
         )
 
         self.final_graph = final_graph.optimize()
