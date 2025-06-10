0573bab5afd7b98aa045e36eb2a90d58983a3311
Jinwoo Bae <bbae7050@gmail.com>
2025-06-10 07:04:16 -0700

    Add Korean TN support for cardinal numbers and postprocessing (#285)
    * Add Korean TN support for cardinal numbers and postprocessing
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Refactor Korean TN cardinal and postprocessing logic based on review feedback
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add __init__.py to ko/data directory
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * Update KO_TN_CACHE to trigger Korean CI run
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    ---------
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>

---
+++ nemo_text_processing/text_normalization/ko/verbalizers/post_processing.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+
+import pynini
+
+from nemo_text_processing.text_normalization.en.graph_utils import NEMO_SIGMA, generator_main
+from nemo_text_processing.utils.logging import logger
+
+
+class PostProcessingFst:
+    """
+    Finite state transducer that post-processing an entire sentence after verbalization is complete, e.g.
+    removes extra spaces around punctuation marks " ( one hundred and twenty three ) " -> "(one hundred and twenty three)"
+
+    Args:
+        cache_dir: path to a dir with .far grammar file. Set to None to avoid using cache.
+        overwrite_cache: set to True to overwrite .far files
+    """
+
+    def __init__(self, cache_dir: str = None, overwrite_cache: bool = False):
+
+        far_file = None
+        if cache_dir is not None and cache_dir != "None":
+            os.makedirs(cache_dir, exist_ok=True)
+            far_file = os.path.join(cache_dir, "ko_tn_post_processing.far")
+        if not overwrite_cache and far_file and os.path.exists(far_file):
+            self.fst = pynini.Far(far_file, mode="r")["post_process_graph"]
+            logger.info(f'Post processing graph was restored from {far_file}.')
+        else:
+            self.fst = self.get_postprocess_graph()
+
+            if far_file:
+                generator_main(far_file, {"post_process_graph": self.fst})
+
+    def get_postprocess_graph(self):
+        return pynini.cdrewrite(pynini.cross("", ""), "", "", pynini.closure(NEMO_SIGMA)).optimize()
