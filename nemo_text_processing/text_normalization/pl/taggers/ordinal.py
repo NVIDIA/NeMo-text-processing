89d7ce03b40f3c59a451ef3b2fd11f711f12b889
Jim O'Regan <joregan@kth.se>
2025-07-30 15:43:30 +0000

    move function

--- nemo_text_processing/text_normalization/pl/taggers/ordinal.py
+++ nemo_text_processing/text_normalization/pl/taggers/ordinal.py
@@ -18,65 +18,7 @@ from nemo_text_processing.text_normalization.pl.utils import get_abs_path, load_
 # from nemo_text_processing.text_normalization.pl.taggers.cardinal import cardinal_graph
 from pynini.lib import pynutil
 
-
-def adjective_inflection(word: str, compound: str = "") -> dict:
-    def fill_bare_template(stem, mi_sg, mp_pl, vowel, stem_b="", compound=""):
-        if stem_b == "":
-            stem_b = stem
-        if compound == "":
-            compound = stem_b + "o"
-        return {
-            "mi_sg_nom": mi_sg,
-            "mi_sg_gen": stem + "ego",
-            "mi_sg_dat": stem + "emu",
-            "mi_sg_ins": stem + vowel + "m",
-            "nt_sg_nom": stem + "e",
-            "f_sg_nom": stem_b + "a",
-            "f_sg_gen": stem + "ej",
-            "f_sg_ins": stem_b + "ą",
-            "mp_pl_nom": mp_pl,
-            "pl_ins": stem + vowel + "mi",
-            "pl_loc": stem + vowel + "ch",
-            "compound": compound,
-        }
-    stem_b = ""
-    if word.endswith("en"):
-        stem = word[:-2] + "n"
-        mi_sg = word
-        mp_pl = stem + "i"
-        vowel = "y"
-    elif word[-2:] in ["ni", "ci"]:
-        stem = word
-        mi_sg = word
-        mp_pl = word
-        vowel = ""
-    elif word.endswith("szy"):
-        stem = word[:-1]
-        mi_sg = word
-        mp_pl = word[:-2] + "i"
-        vowel = "y"
-    elif word.endswith("gi"):
-        stem = word
-        stem_b = word[:-1]
-        mi_sg = word
-        mp_pl = word[:-2] + "dzy"
-        vowel = ""
-    elif word.endswith("sty"):
-        stem = word[:-1]
-        mi_sg = word
-        mp_pl = word[:-3] + "ści"
-        vowel = "y"
-    elif word.endswith("ty"):
-        stem = word[:-1]
-        mi_sg = word
-        mp_pl = word[:-2] + "ci"
-        vowel = "y"
-    elif word.endswith("y"):
-        stem = word[:-1]
-        mi_sg = word
-        mp_pl = word[:-1] + "i"
-        vowel = "y"
-    return fill_bare_template(stem, mi_sg, mp_pl, vowel, stem_b, compound)
+from nemo_text_processing.text_normalization.pl.utils import adjective_inflection
 
 
 def complete_paradigm(partial, complete=False):
--- nemo_text_processing/text_normalization/pl/utils.py
+++ nemo_text_processing/text_normalization/pl/utils.py
@@ -40,3 +40,63 @@ def load_labels(abs_path):
     with open(abs_path, encoding="utf-8") as label_tsv:
         labels = list(csv.reader(label_tsv, delimiter="\t"))
         return labels
+
+
+def adjective_inflection(word: str, compound: str = "") -> dict:
+    def fill_bare_template(stem, mi_sg, mp_pl, vowel, stem_b="", compound=""):
+        if stem_b == "":
+            stem_b = stem
+        if compound == "":
+            compound = stem_b + "o"
+        return {
+            "mi_sg_nom": mi_sg,
+            "mi_sg_gen": stem + "ego",
+            "mi_sg_dat": stem + "emu",
+            "mi_sg_ins": stem + vowel + "m",
+            "nt_sg_nom": stem + "e",
+            "f_sg_nom": stem_b + "a",
+            "f_sg_gen": stem + "ej",
+            "f_sg_ins": stem_b + "ą",
+            "mp_pl_nom": mp_pl,
+            "pl_ins": stem + vowel + "mi",
+            "pl_loc": stem + vowel + "ch",
+            "compound": compound,
+        }
+    stem_b = ""
+    if word.endswith("en"):
+        stem = word[:-2] + "n"
+        mi_sg = word
+        mp_pl = stem + "i"
+        vowel = "y"
+    elif word[-2:] in ["ni", "ci"]:
+        stem = word
+        mi_sg = word
+        mp_pl = word
+        vowel = ""
+    elif word.endswith("szy"):
+        stem = word[:-1]
+        mi_sg = word
+        mp_pl = word[:-2] + "i"
+        vowel = "y"
+    elif word.endswith("gi"):
+        stem = word
+        stem_b = word[:-1]
+        mi_sg = word
+        mp_pl = word[:-2] + "dzy"
+        vowel = ""
+    elif word.endswith("sty"):
+        stem = word[:-1]
+        mi_sg = word
+        mp_pl = word[:-3] + "ści"
+        vowel = "y"
+    elif word.endswith("ty"):
+        stem = word[:-1]
+        mi_sg = word
+        mp_pl = word[:-2] + "ci"
+        vowel = "y"
+    elif word.endswith("y"):
+        stem = word[:-1]
+        mi_sg = word
+        mp_pl = word[:-1] + "i"
+        vowel = "y"
+    return fill_bare_template(stem, mi_sg, mp_pl, vowel, stem_b, compound)
