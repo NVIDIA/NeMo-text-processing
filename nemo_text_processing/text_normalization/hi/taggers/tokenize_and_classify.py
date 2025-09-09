278be1c2441c90714355a536d7872d29fd7322f3
shreeshd-tn <shreeshd@nvidia.com>
2025-09-09 20:57:47 +0530

    Hindi TN 2.0 - Telephone class integration from staging branch (#320)
    * telephone class integration
    
    (cherry picked from commit a7c9adf48038c95336419eb02543d53a50538e03)
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * Updated date in Jenkins file to the PR creation date
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * Jenkins file date change
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * Trying today's date
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * improved country code coverage + some test cases
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Ignore test generated files
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * Improved landline detection and added edge test cases for proper coverage
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Deleted gitignore file
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    
    ---------
    
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: shreeshd-tn <shreeshd@nvidia.com>

--- nemo_text_processing/text_normalization/hi/taggers/tokenize_and_classify.py
+++ nemo_text_processing/text_normalization/hi/taggers/tokenize_and_classify.py
@@ -20,6 +20,7 @@ import pynini
 from pynini.lib import pynutil
 
 from nemo_text_processing.text_normalization.hi.graph_utils import (
+    NEMO_SPACE,
     NEMO_WHITE_SPACE,
     GraphFst,
     delete_extra_space,
@@ -33,6 +34,7 @@ from nemo_text_processing.text_normalization.hi.taggers.fraction import Fraction
 from nemo_text_processing.text_normalization.hi.taggers.measure import MeasureFst
 from nemo_text_processing.text_normalization.hi.taggers.money import MoneyFst
 from nemo_text_processing.text_normalization.hi.taggers.punctuation import PunctuationFst
+from nemo_text_processing.text_normalization.hi.taggers.telephone import TelephoneFst
 from nemo_text_processing.text_normalization.hi.taggers.time import TimeFst
 from nemo_text_processing.text_normalization.hi.taggers.whitelist import WhiteListFst
 from nemo_text_processing.text_normalization.hi.taggers.word import WordFst
@@ -123,6 +125,11 @@ class ClassifyFst(GraphFst):
             punct_graph = punctuation.fst
             logging.debug(f"punct: {time.time() - start_time: .2f}s -- {punct_graph.num_states()} nodes")
 
+            start_time = time.time()
+            telephone = TelephoneFst()
+            telephone_graph = telephone.fst
+            logging.debug(f"telephone: {time.time() - start_time: .2f}s -- {telephone_graph.num_states()} nodes")
+
             classify = (
                 pynutil.add_weight(whitelist_graph, 1.01)
                 | pynutil.add_weight(cardinal_graph, 1.1)
@@ -132,6 +139,7 @@ class ClassifyFst(GraphFst):
                 | pynutil.add_weight(time_graph, 1.1)
                 | pynutil.add_weight(measure_graph, 1.1)
                 | pynutil.add_weight(money_graph, 1.1)
+                | pynutil.add_weight(telephone_graph, 1.1)
             )
 
             start_time = time.time()
@@ -141,20 +149,22 @@ class ClassifyFst(GraphFst):
             punct = pynutil.insert("tokens { ") + pynutil.add_weight(punct_graph, weight=2.1) + pynutil.insert(" }")
             punct = pynini.closure(
                 pynini.compose(pynini.closure(NEMO_WHITE_SPACE, 1), delete_extra_space)
-                | (pynutil.insert(" ") + punct),
+                | (pynutil.insert(NEMO_SPACE) + punct),
                 1,
             )
 
             classify |= pynutil.add_weight(word_graph, 100)
             token = pynutil.insert("tokens { ") + classify + pynutil.insert(" }")
             token_plus_punct = (
-                pynini.closure(punct + pynutil.insert(" ")) + token + pynini.closure(pynutil.insert(" ") + punct)
+                pynini.closure(punct + pynutil.insert(NEMO_SPACE))
+                + token
+                + pynini.closure(pynutil.insert(NEMO_SPACE) + punct)
             )
 
             graph = token_plus_punct + pynini.closure(
                 (
                     pynini.compose(pynini.closure(NEMO_WHITE_SPACE, 1), delete_extra_space)
-                    | (pynutil.insert(" ") + punct + pynutil.insert(" "))
+                    | (pynutil.insert(NEMO_SPACE) + punct + pynutil.insert(NEMO_SPACE))
                 )
                 + token_plus_punct
             )
