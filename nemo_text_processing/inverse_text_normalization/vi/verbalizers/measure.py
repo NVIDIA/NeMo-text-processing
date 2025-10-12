79067676a1aba0150f50f8bf1f1662ba0ead0a7e
Mai Anh <palasek182@gmail.com>
2025-10-13 04:12:37 +0700

    Date time itn (#333)
    * improve numeric semiotic classes
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * Fix Jenkinsfile for CI (#325)
    
    * Fix Jenkinsfile for CI
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Fix requirements for test
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Update paths and docker
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Fix docker name
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Fix click version
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Change path of grammars for sparrowhawk tests
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Update paths in sh_test.sh
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Update paths
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    * Revert paths
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    
    ---------
    
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * revert old codes
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * revert not inherit
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * improve date time
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix pynini union instead of union operator
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * improve measure, telephone, electronic
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * change union operator to pynini union
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Mai Anh <palasek182@gmail.com>
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>
    Co-authored-by: anand-nv <105917641+anand-nv@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Mariana Graterol Fuenmayor <marianag@nvidia.com>
    Signed-off-by: Mai Anh <palasek182@gmail.com>

--- nemo_text_processing/inverse_text_normalization/vi/verbalizers/measure.py
+++ nemo_text_processing/inverse_text_normalization/vi/verbalizers/measure.py
@@ -17,10 +17,11 @@ import pynini
 from pynini.lib import pynutil
 
 from nemo_text_processing.inverse_text_normalization.vi.graph_utils import (
-    NEMO_CHAR,
     NEMO_NOT_QUOTE,
+    NEMO_NOT_SPACE,
     GraphFst,
     delete_space,
+    insert_space,
 )
 
 
@@ -37,11 +38,23 @@ class MeasureFst(GraphFst):
     def __init__(self, decimal: GraphFst, cardinal: GraphFst):
         super().__init__(name="measure", kind="verbalize")
         optional_sign = pynini.closure(pynini.cross('negative: "true"', "-"), 0, 1)
-        unit = (
+        # Units that don't need space (time units)
+        no_space_units = pynini.union("s", "ms", "ns", "μs", "h", "min", "%")
+
+        unit_no_space = (
             pynutil.delete("units:")
             + delete_space
             + pynutil.delete('"')
-            + pynini.closure(NEMO_CHAR - " ", 1)
+            + no_space_units
+            + pynutil.delete('"')
+            + delete_space
+        )
+
+        unit_with_space = (
+            pynutil.delete("units:")
+            + delete_space
+            + pynutil.delete('"')
+            + (pynini.closure(NEMO_NOT_SPACE, 1) - no_space_units)
             + pynutil.delete('"')
             + delete_space
         )
@@ -72,13 +85,25 @@ class MeasureFst(GraphFst):
             + pynutil.delete('"')
         )
         optional_fractional = pynini.closure(fractional + delete_space, 0, 1)
-        graph = (
-            (graph_cardinal | graph_decimal)
+        # Graph with no space for time units
+        graph_no_space = (
+            pynini.union(graph_cardinal, graph_decimal)
             + delete_space
             + optional_fractional
-            + pynutil.insert(" ")
-            + unit
+            + unit_no_space
             + delete_space
         )
+
+        # Graph with space for other units
+        graph_with_space = (
+            pynini.union(graph_cardinal, graph_decimal)
+            + delete_space
+            + optional_fractional
+            + insert_space
+            + unit_with_space
+            + delete_space
+        )
+
+        graph = pynini.union(graph_no_space, graph_with_space)
         delete_tokens = self.delete_tokens(graph)
         self.fst = delete_tokens.optimize()
