842256ad82326210ff8547af7bb2263c4d4e6c2c
tbartley94 <tbartley@nvidia.com>
2025-11-18 12:10:58 -0800

    Hebrew itn (#366)
    * rebase to main
    
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rebase
    
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rebasing
    
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * responding to formatting pr
    
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    
    * isort and moving string map to string file
    
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: tbartley94 <tbartley@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nemo_text_processing/inverse_text_normalization/inverse_normalize.py
+++ nemo_text_processing/inverse_text_normalization/inverse_normalize.py
@@ -131,6 +131,11 @@ class InverseNormalizer(Normalizer):
             from nemo_text_processing.inverse_text_normalization.ja.verbalizers.verbalize_final import (
                 VerbalizeFinalFst,
             )
+        elif lang == 'he':  # Japanese
+            from nemo_text_processing.inverse_text_normalization.he.taggers.tokenize_and_classify import ClassifyFst
+            from nemo_text_processing.inverse_text_normalization.he.verbalizers.verbalize_final import (
+                VerbalizeFinalFst,
+            )
 
         self.tagger = ClassifyFst(
             cache_dir=cache_dir, whitelist=whitelist, overwrite_cache=overwrite_cache, input_case=input_case
@@ -175,7 +180,7 @@ def parse_args():
     parser.add_argument(
         "--language",
         help="language",
-        choices=['en', 'de', 'es', 'pt', 'ru', 'fr', 'sv', 'vi', 'ar', 'es_en', 'zh', 'hi', 'hy', 'mr', 'ja'],
+        choices=['en', 'de', 'es', 'pt', 'ru', 'fr', 'sv', 'vi', 'ar', 'es_en', 'zh', 'he', 'hi', 'hy', 'mr', 'ja'],
         default="en",
         type=str,
     )
