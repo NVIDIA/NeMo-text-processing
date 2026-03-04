acb1cd8cb3cdfd30209295b3e688ede6843f7ecd
Jinwoo Bae <bbae7050@gmail.com>
2026-03-04 14:27:45 -0800

    Korean TN for Date and Time (#316)
    * feat(ko/date): Add date TN taggers, verbalizers, test cases, and post-processing fixes
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix(ko/date): update date tagger and sparrowhawk test
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ko(TN): Date TN fixes & cleanup
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ko(TN): Add Time tagger/verbalizer + tests
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ko(TN): Date — strict YYYY for delimited formats; define single-year 1–4 digit behavior
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Jinwoo Bae <bbae7050@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ nemo_text_processing/text_normalization/ko/verbalizers/time.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2025 NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pynini
+from pynini.lib import pynutil
+
+from nemo_text_processing.text_normalization.ko.graph_utils import NEMO_NOT_QUOTE, GraphFst, delete_space, insert_space
+
+
+class TimeFst(GraphFst):
+    """
+    Finite state transducer for verbalizing time e.g.
+
+
+    """
+
+    def __init__(self, deterministic: bool = True):
+        super().__init__(name="time", kind="verbalize", deterministic=deterministic)
+
+        hour_component = pynutil.delete("hours: \"") + pynini.closure(NEMO_NOT_QUOTE) + pynutil.delete("\"")
+        minute_content = pynini.closure(NEMO_NOT_QUOTE)
+        minute_component = pynutil.delete("minutes: \"") + pynini.cross("영분", "") + pynutil.delete(
+            "\""
+        ) | pynutil.delete("minutes: \"") + (minute_content - "영분") + pynutil.delete("\"")
+
+        second_content = pynini.closure(NEMO_NOT_QUOTE)
+        second_component = pynutil.delete("seconds: \"") + pynini.cross("영초", "") + pynutil.delete(
+            "\""
+        ) | pynutil.delete("seconds: \"") + (second_content - "영초") + pynutil.delete("\"")
+
+        division_component = pynutil.delete("suffix: \"") + pynini.closure(NEMO_NOT_QUOTE) + pynutil.delete("\"")
+
+        graph_basic_time = pynini.closure(division_component + delete_space + insert_space, 0, 1) + (
+            (
+                hour_component
+                + delete_space
+                + insert_space
+                + minute_component
+                + delete_space
+                + insert_space
+                + second_component
+            )
+            | (hour_component + delete_space + insert_space + minute_component)
+            | hour_component
+            | minute_component
+            | second_component
+        )
+
+        final_graph = graph_basic_time
+
+        delete_tokens = self.delete_tokens(final_graph)
+        self.fst = delete_tokens.optimize()
---
+++ tests/nemo_text_processing/ko/test_time.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from parameterized import parameterized
+
+from nemo_text_processing.text_normalization.normalize import Normalizer
+from ..utils import parse_test_case_file
+
+
+class TestTime:
+    normalizer_ko = Normalizer(
+        lang='ko', cache_dir='export/ko_tn_grammars_lower_cased', overwrite_cache=False, input_case='lower_cased'
+    )
+
+    @parameterized.expand(parse_test_case_file('ko/data_text_normalization/test_cases_time.txt'))
+    @pytest.mark.run_only_on('CPU')
+    @pytest.mark.unit
+    def test_time(self, test_input, expected):
+        preds = self.normalizer_ko.normalize(test_input)
+        assert expected == preds
