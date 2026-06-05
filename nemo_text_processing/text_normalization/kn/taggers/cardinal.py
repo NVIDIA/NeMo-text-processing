62b7a5806f1037f901fbadd53e24773c1ed38061
richa-2002 <richa@nvidia.com>
2026-06-05 15:25:34 +0530

    feat(kn): add cardinal TN tagger, verbalizer and test cases
    Signed-off-by: richa-2002 <richa@nvidia.com>

--- nemo_text_processing/text_normalization/kn/taggers/cardinal.py
+++ nemo_text_processing/text_normalization/kn/taggers/cardinal.py
@@ -1,10 +1,22 @@
-# copyright (c) 2026 NVIDIA Corporation
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pynini 
 from pynini.lib import pynutil 
  
 from nemo_text_processing.text_normalization.kn.graph_utils import GraphFst 
 from nemo_text_processing.text_normalization.kn.utils import get_abs_path 
- 
  
 class CardinalFst(GraphFst): 
     """ 
@@ -18,13 +30,10 @@ class CardinalFst(GraphFst):
         digit = pynini.string_file(get_abs_path("data/numbers/digit.tsv")) 
         zero = pynini.string_file(get_abs_path("data/numbers/zero.tsv")) 
         teens_and_ties = pynini.string_file(get_abs_path("data/numbers/teens_and_ties.tsv")) 
-        #hundreds = pynini.string_file(get_abs_path("data/numbers/hundreds.tsv"))
  
-        
-        graph = digit | zero | teens_and_ties             # <-- complete this 
+        graph = digit | zero | teens_and_ties            # <-- complete this 
         graph = graph.optimize() 
  
-        
         final_graph = pynutil.insert('integer: "') + graph + pynutil.insert('"')  
  
         # add_tokens() turns it into:   cardinal { integer: "<word>" } 
