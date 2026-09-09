0f44929b10eafefe4e698c23cf071b03814162de
Haridas Nishitha <nharidas@nvidia.com>
2026-09-09 13:41:19 +0530

    feat(te): Updated.
    Signed-off-by: Haridas Nishitha <nharidas@nvidia.com>

--- tests/nemo_text_processing/te/test_sparrowhawk_normalization.sh
+++ tests/nemo_text_processing/te/test_sparrowhawk_normalization.sh
@@ -28,5 +28,15 @@ testTNCardinal() {
   runtest $input
 }
 
+testTNWord() {
+  input=$PROJECT_DIR/te/data_text_normalization/test_cases_word.txt
+  runtest $input
+}
+
+testTNPunctuation() {
+  input=$PROJECT_DIR/te/data_text_normalization/test_cases_punctuation.txt
+  runtest $input
+}
+
 # Load shUnit2
 . $PROJECT_DIR/../shunit2/shunit2
