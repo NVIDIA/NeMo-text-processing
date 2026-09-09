86a9b041304bb8bb85a5bc1b742ed64762f666a3
surendran-246 <surendrans@nvidia.com>
2026-09-09 15:59:52 +0530

    Update sparrowhawk file
    Signed-off-by: surendran-246 <surendrans@nvidia.com>

--- tests/nemo_text_processing/ta/test_sparrowhawk_normalization.sh
+++ tests/nemo_text_processing/ta/test_sparrowhawk_normalization.sh
@@ -27,6 +27,14 @@ testTNCardinal() {
   input=$PROJECT_DIR/ta/data_text_normalization/test_cases_cardinal.txt
   runtest $input
 }
-
+testTNWord() {
+  input=$PROJECT_DIR/ta/data_text_normalization/test_cases_word.txt
+  runtest $input
+}
+ 
+testTNPunctuation() {
+  input=$PROJECT_DIR/ta/data_text_normalization/test_cases_punctuation.txt
+  runtest $input
+}
 # Load shUnit2
 . $PROJECT_DIR/../shunit2/shunit2
