879a73dd6409dd84034ed5515372ef281e7ad898
Shreyas Pawar <shrpawar@nvidia.com>
2026-09-18 12:37:24 +0000

    review feedback changes 2
    Signed-off-by: Shreyas Pawar <shrpawar@nvidia.com>

--- nemo_text_processing/text_normalization/hi/taggers/time.py
+++ nemo_text_processing/text_normalization/hi/taggers/time.py
@@ -65,13 +65,18 @@ class TimeFst(GraphFst):
         self.minutes = pynutil.insert("minutes: \"") + minutes_graph + pynutil.insert("\" ")
         self.seconds = pynutil.insert("seconds: \"") + seconds_graph + pynutil.insert("\" ")
 
-        # hour minute seconds
+       # hour minute seconds (allows 00 minutes and 00 seconds)
         graph_hms = (
             self.hours + delete_colon + insert_space + self.minutes + delete_colon + insert_space + self.seconds
         )
 
+        # Restrict graph_hm from accepting 00 minutes so H:00 falls back to graph_h
+        exclude_double_zero = pynini.union("00", "००").optimize()
+        minutes_no_zero_graph = pynini.difference(pynini.project(minutes_graph, "input"), exclude_double_zero) @ minutes_graph
+        hm_minutes_restricted = pynutil.insert("minutes: \"") + minutes_no_zero_graph + pynutil.insert("\" ")
+
         # hour minute
-        graph_hm = self.hours + delete_colon + insert_space + self.minutes
+        graph_hm = self.hours + delete_colon + insert_space + hm_minutes_restricted
 
         # hour
         graph_h = self.hours + delete_colon + pynutil.delete(HI_DOUBLE_ZERO)
@@ -94,8 +99,9 @@ class TimeFst(GraphFst):
         savva_graph = pynutil.insert(HI_SAVVA) + pynutil.insert(NEMO_SPACE) + savva_numbers
 
         # Restrict 'sadhe' from accepting 1 or 2 so it doesn't conflict with dedh/dhai
-        exclude_dedh_dhai = pynini.union("1", "१", "01", "०१", "2", "२", "02", "०२").optimize()
-
+        exclude_tsv = pynini.string_file(get_abs_path("data/time/exclude_dedh_dhai.tsv"))
+        exclude_dedh_dhai = pynini.project(exclude_tsv, "input").optimize()
+        
         # Project cardinal_graph to an acceptor, subtract exceptions, then compose (@) back to the transducer
         valid_sadhe_inputs = pynini.difference(pynini.project(cardinal_graph, "input"), exclude_dedh_dhai)
         sadhe_cardinal = valid_sadhe_inputs @ cardinal_graph
